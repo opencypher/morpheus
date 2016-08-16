99d1c82f439618e2d1bfbd0e52c58011d5cdbc3e
Stefan Plantikow <stefan.plantikow@googlemail.com>
2016-08-17 01:25:53 +0200

    Add escaping support

--- src/main/scala/org/opencypher/spark/impl/StdField.scala
+++ src/main/scala/org/opencypher/spark/impl/StdField.scala
@@ -3,11 +3,47 @@ package org.opencypher.spark.impl
 import org.opencypher.spark.api.{CypherField, CypherType}
 
 object StdField {
-
   def apply(pair: (Symbol, CypherType)): StdField = new StdField(pair._1, pair._2)
 }
 
-case class StdField(sym: Symbol, cypherType: CypherType) extends CypherField {
-  // TODO: Properly escape here
-  val columnSym: Symbol = Symbol(sym.name.filterNot(_ == '.').filterNot( _ == '(').filterNot(_ == ')'))
+final case class StdField(sym: Symbol, cypherType: CypherType) extends CypherField {
+  def column = SparkIdentifier(sym.name)
+}
+
+object SparkIdentifier {
+
+  def apply(name: String): SparkIdentifier = {
+    val builder = new StringBuilder(name.length + 16)
+    name.foreach {
+      case ch if isUnescapedChar(ch) => builder.append(ch)
+      case '-' => builder.append("--")
+      case '#' => builder.append("-hash-")
+      case '@' => builder.append("-at-")
+      case '`' => builder.append("-backtick-")
+      case ';' => builder.append("-semicolon-")
+      case '\'' => builder.append("-tick-")
+      case '\"' => builder.append("-quote-")
+      case '/' => builder.append("-slash-")
+      case '\\' => builder.append("-backslash-")
+      case '\t' => builder.append("-tab-")
+      case '\n' => builder.append("-nl-")
+      case '\r' => builder.append("-ff-")
+      case ch => builder.append(s"-U+${Integer.toHexString(ch.toInt)}-")
+    }
+    SparkIdentifier(builder.result())
+  }
+
+  def isEscapedIdentifierName(name: String) =
+    name.length > 0 && Character.isLetter(name.charAt(0)) && name.forall(isRegularChar)
+
+  private def isUnescapedChar(ch: Char) =
+    Character.isLetterOrDigit(ch) || "()[]<>.:+*/|&$".contains(ch)
+
+  private def isRegularChar(ch: Char) =
+    Character.isLetterOrDigit(ch) || ch == '-'
+}
+
+final class SparkIdentifier private[SparkIdentifier](val name: String) extends AnyVal {
+  def ++(other: SparkIdentifier) = new SparkIdentifier(s"$name-$other")
+  def symbol = Symbol(if (SparkIdentifier.isEscapedIdentifierName(name)) name else s"`$name`")
 }
--- src/main/scala/org/opencypher/spark/impl/util/SlotSymbolGenerator.scala
+++ src/main/scala/org/opencypher/spark/impl/util/SlotSymbolGenerator.scala
@@ -1,13 +1,12 @@
 package org.opencypher.spark.impl.util
 
-import org.opencypher.spark.impl.StdField
+import org.opencypher.spark.impl.{SparkIdentifier, StdField}
 
 class SlotSymbolGenerator {
   private var id = 0
 
-  // TODO: escape weird characters
   def newSlotSymbol(field: StdField): Symbol = {
     id += 1
-    Symbol(s"${field.columnSym.name}$id")
+    (field.column ++ SparkIdentifier(id.toString)).symbol
   }
 }
