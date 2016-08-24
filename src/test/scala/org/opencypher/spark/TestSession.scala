2598a7b1bc1a22882675bfcb29b9c67b6a345002
Stefan Plantikow <stefan.plantikow@googlemail.com>
2016-08-24 21:24:35 +0200

    Add note detailing that running tests on Java 7 is slow due to a JDK krb5 bug

--- src/test/scala/org/opencypher/spark/TestSession.scala
+++ src/test/scala/org/opencypher/spark/TestSession.scala
@@ -1,6 +1,9 @@
 package org.opencypher.spark
 
+import java.util.UUID
+
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.{SparkConf, SparkContext}
 import org.scalatest.{BeforeAndAfterEach, FunSuite}
 
 object TestSession {
@@ -21,5 +24,15 @@ object TestSession {
 }
 
 object TestSessionFactory {
-  def create = SparkSession.builder().master("local[4]").getOrCreate()
+  def create = {
+    //
+    // If this is slow, you might be hitting: http://bugs.java.com/view_bug.do?bug_id=8077102
+    //
+    SparkSession
+      .builder()
+      .config(new SparkConf(true))
+      .master("local[*]")
+      .appName(s"cypher-on-spark-tests-${UUID.randomUUID()}")
+      .getOrCreate()
+  }
 }
