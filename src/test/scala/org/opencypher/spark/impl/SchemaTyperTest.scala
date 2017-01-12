b87b9deb87bba2849cde053fc53fbd4789a12953
Mats Rydberg <mats@neotechnology.com>
2017-01-12 15:26:39 +0100

    Shape up test code to use DSL

--- src/test/scala/org/opencypher/spark/impl/SchemaTyperTest.scala
+++ src/test/scala/org/opencypher/spark/impl/SchemaTyperTest.scala
@@ -7,7 +7,7 @@ import org.opencypher.spark.StdTestSuite
 import org.opencypher.spark.api.CypherType
 import org.opencypher.spark.api.schema.StdSchema
 import org.opencypher.spark.api.types._
-import org.opencypher.spark.impl.types.{UntypedExpression, SchemaTyper, TypeContext, TypingFailed}
+import org.opencypher.spark.impl.types._
 import org.parboiled.scala._
 
 class SchemaTyperTest extends StdTestSuite with AstConstructionTestSupport {
@@ -17,101 +17,36 @@ class SchemaTyperTest extends StdTestSuite with AstConstructionTestSupport {
     .withRelationshipKeys("KNOWS")("since" -> CTInteger, "relative" -> CTBoolean)
   val typer = SchemaTyper(schema)
 
-  test("typing of simple function") {
-    val context = TypeContext.empty
-    val expr = parse("timestamp()")
+  test("typing of functions") {
+    assertExpr("timestamp()") hasType CTInteger
+    assertExpr("toInteger(1.0)") hasType CTInteger
+    assertExpr("size([0, true, []])") hasType CTInteger
 
-    typer.infer(expr, context) match {
-      case result: TypeContext =>
-        result.typeTable should contain(expr -> CTInteger)
-    }
-  }
-
-  test("typing of function with multiple similar signatures") {
-    val expr = parse("toInteger(1.0)")
-
-    typer.infer(expr, TypeContext.empty) match {
-      case result: TypeContext =>
-        result.typeTable should contain(expr -> CTInteger)
-    }
-  }
-
-  test("typing of function with multiple different signatures") {
-    val expr = "size([0, true, []])"
-
-    typer.infer(parse(expr), TypeContext.empty) match {
-      case result: TypeContext =>
-        result.typeTable should contain(parse(expr) -> CTInteger)
-    }
-  }
-
-  test("typing of function with multiple signatures with different output types") {
-    val intFloatArgs = parse("percentileDisc(1, 3.14)")
-    typer.infer(intFloatArgs, TypeContext.empty) match {
-      case result: TypeContext =>
-        result.typeTable should contain(intFloatArgs -> CTInteger)
-    }
-
-    val floatFloatArgs = parse("percentileDisc(6.67, 3.14)")
-    typer.infer(floatFloatArgs, TypeContext.empty) match {
-      case result: TypeContext =>
-        result.typeTable should contain(floatFloatArgs -> CTFloat)
-    }
-
-    val numberFloatArgs = parse("percentileDisc([1, 3.14][0], 3.14)")
-    typer.infer(numberFloatArgs, TypeContext.empty) match {
-      case result: TypeContext =>
-        result.typeTable should contain(numberFloatArgs -> CTNumber)
-    }
+    assertExpr("percentileDisc(1, 3.14)") hasType CTInteger
+    assertExpr("percentileDisc(6.67, 3.14)") hasType CTFloat
+    assertExpr("percentileDisc([1, 3.14][0], 3.14)") hasType CTNumber
   }
 
   test("typing of list indexing") {
-    val intList = parse("[1, 2][15]")
-    typer.infer(intList, TypeContext.empty) match {
-      case result: TypeContext =>
-        result.typeTable should contain(intList -> CTInteger)
-    }
-
-    val numberList = parse("[3.14, -1, 5000][15]")
-    typer.infer(numberList, TypeContext.empty) match {
-      case result: TypeContext =>
-        result.typeTable should contain(numberList -> CTNumber)
-    }
-
-    val mixedList = parse("[[], 1, true][15]")
-    typer.infer(mixedList, TypeContext.empty) match {
-      case result: TypeContext =>
-        result.typeTable should contain(mixedList -> CTAny)
-    }
+    assertExpr("[1, 2][15]") hasType CTInteger
+    assertExpr("[3.14, -1, 5000][15]") hasType CTNumber
+    assertExpr("[[], 1, true][15]") hasType CTAny
   }
 
   test("infer type of node property lookup") {
     val context = TypeContext.empty.updateType(varFor("n") -> CTNode("Person"))
-    val expr = parse("n.name")
 
-    typer.infer(expr, context) match {
-      case result: TypeContext =>
-        result.typeTable should contain(expr -> CTString)
-    }
+    assertExpr("n.name", context) hasType CTString
   }
 
   test("infer type of relationship property lookup") {
     val context = TypeContext.empty.updateType(varFor("r") -> CTRelationship("KNOWS"))
-    val expr = parse("r.relative")
 
-    typer.infer(expr, context) match {
-      case result: TypeContext =>
-        result.typeTable should contain(prop("r", "relative") -> CTBoolean)
-    }
+    assertExpr("r.relative", context) hasType CTBoolean
   }
 
-  test("report missing variable") {
-    val context = TypeContext.empty
-    val expr = parse("r.relative")
-
-    typer.infer(expr, context) match {
-      case TypingFailed(errors) => errors should contain(UntypedExpression(varFor("r")))
-    }
+  test("report untyped expressions") {
+    assertExpr("r.relative") hasErrors UntypedExpression(varFor("r"))
   }
 
   test("basic literals") {
@@ -133,15 +68,24 @@ class SchemaTyperTest extends StdTestSuite with AstConstructionTestSupport {
     assertExpr("[1, 1.0, null]") hasType CTList(CTNumber.nullable)
   }
 
-  private case class assertExpr(exprText: String)  {
+  private case class assertExpr(exprText: String, context: TypeContext = TypeContext.empty)  {
     def hasType(t: CypherType) = {
-      val context = TypeContext.empty
       val expr = parse(exprText)
       typer.infer(expr, context) match {
         case result: TypeContext =>
           result.typeTable should contain(expr -> t)
         case _ =>
           fail(s"Failed to type $exprText")
+      }
+    }
+
+    def hasErrors(expected: TypingError*) = {
+      val expr = parse(exprText)
+      typer.infer(expr, context) match {
+        case TypingFailed(actual) =>
+          actual should equal(expected.toSeq)
+        case _: TypeContext =>
+          fail("Expected to get typing errors, but succeeded")
       }
     }
   }
