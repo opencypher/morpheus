f9cd887627aae3d22df460c6d1d7ad2b851b1f0f
Mats Rydberg <mats@neotechnology.com>
2017-03-16 15:39:08 +0100

    Always clean artifacts on running script

--- cypher-on-spark.sh
+++ cypher-on-spark.sh
@@ -3,7 +3,7 @@ BASEDIR=$(dirname "$0")
 
 function run() {
   cd $BASEDIR
-  mvn compile validate
+  mvn clean compile validate
   VERSION=`cat target/classes/version.txt`
   env MAVEN_OPTS="-Xmx6g -Xms2g -Xss4m -Xverify:none -XX:MaxPermSize=1024m -XX:+UseG1GC -XX:+AggressiveOpts" mvn "-Dproject.version=$VERSION" exec:java
 }
