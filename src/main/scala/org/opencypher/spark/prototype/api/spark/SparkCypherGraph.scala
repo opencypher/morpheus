6cf71e926090428f544215e29d62a97ce8deeaf2
Stefan Plantikow <stefan.plantikow@googlemail.com>
2017-03-18 20:19:53 +0100

    Showcase union on graphs API

--- src/main/scala/org/opencypher/spark/prototype/api/spark/SparkCypherGraph.scala
+++ src/main/scala/org/opencypher/spark/prototype/api/spark/SparkCypherGraph.scala
@@ -8,9 +8,39 @@ import org.opencypher.spark.prototype.api.record.{OpaqueField, RecordHeader}
 import org.opencypher.spark.prototype.api.schema.Schema
 
 trait SparkCypherGraph extends CypherGraph {
+
+  self =>
+
   override type Space = SparkGraphSpace
   override type Graph = SparkCypherGraph
   override type Records = SparkCypherRecords
+
+  def union(other: SparkCypherGraph) = new SparkCypherGraph {
+
+    override def nodes(v: Var) = new SparkCypherGraph {
+      override def nodes(v: Var) = self.nodes(v)
+      override def relationships(v: Var) = SparkCypherGraph.empty(self.space)
+      override def model = QueryModel.nodes(Field(v.name), self.space.globals)
+      override def details = self.nodes(v).details.concat(other.nodes(v).details)
+      override def space = self.space
+      override def schema = self.schema
+    }
+
+    override def relationships(v: Var) = new SparkCypherGraph {
+      override def nodes(v: Var) = self.nodes(v)
+      override def relationships(v: Var) = self.relationships(v)
+      override def model = QueryModel.relationships(Field(v.name), self.space.globals)
+      override def details = self.relationships(v).details.concat(other.relationships(v).details)
+      override def space = self.space
+      override def schema = self.schema
+    }
+
+    override def model = ???
+    override def details = ???
+
+    override def space = self.space
+    override def schema = self.schema
+  }
 }
 
 object SparkCypherGraph {
