ce86735741af79d5d6d9646464847b92075b6a3d
Stefan Plantikow <stefan.plantikow@googlemail.com>
2017-03-18 20:19:53 +0100

    Showcase union and intersect graph API

--- src/main/scala/org/opencypher/spark/prototype/api/spark/SparkCypherGraph.scala
+++ src/main/scala/org/opencypher/spark/prototype/api/spark/SparkCypherGraph.scala
@@ -8,9 +8,66 @@ import org.opencypher.spark.prototype.api.record.{OpaqueField, RecordHeader}
 import org.opencypher.spark.prototype.api.schema.Schema
 
 trait SparkCypherGraph extends CypherGraph {
+
+  self =>
+
   override type Space = SparkGraphSpace
   override type Graph = SparkCypherGraph
   override type Records = SparkCypherRecords
+
+  def union(other: SparkCypherGraph): SparkCypherGraph = new SparkCypherGraph {
+
+    override def nodes(v: Var) = new SparkCypherGraph {
+      override def nodes(v: Var) = self.nodes(v)
+      override def relationships(v: Var) = SparkCypherGraph.empty(self.space)
+      override def model = QueryModel.nodes(Field(v.name), self.space.globals)
+      override def details = self.nodes(v).details.union(other.nodes(v).details)
+      override def space = self.space
+      override def schema = self.schema
+    }
+
+    override def relationships(v: Var) = new SparkCypherGraph {
+      override def nodes(v: Var) = self.nodes(v)
+      override def relationships(v: Var) = self.relationships(v)
+      override def model = QueryModel.relationships(Field(v.name), self.space.globals)
+      override def details = self.relationships(v).details.union(other.relationships(v).details)
+      override def space = self.space
+      override def schema = self.schema
+    }
+
+    override def model = ???
+    override def details = self.details.union(other.details)
+
+    override def space = self.space
+    override def schema = self.schema
+  }
+
+  def intersect(other: SparkCypherGraph): SparkCypherGraph = new SparkCypherGraph {
+
+    override def nodes(v: Var) = new SparkCypherGraph {
+      override def nodes(v: Var) = self.nodes(v).intersect(other.nodes(v))
+      override def relationships(v: Var) = SparkCypherGraph.empty(self.space)
+      override def model = QueryModel.nodes(Field(v.name), self.space.globals)
+      override def details = self.nodes(v).details.intersect(other.nodes(v).details)
+      override def space = self.space
+      override def schema = self.schema
+    }
+
+    override def relationships(v: Var) = new SparkCypherGraph {
+      override def nodes(v: Var) = self.nodes(v).intersect(other.nodes(v))
+      override def relationships(v: Var) = self.relationships(v).intersect(other.relationships(v))
+      override def model = QueryModel.relationships(Field(v.name), self.space.globals)
+      override def details = self.relationships(v).details.intersect(other.relationships(v).details)
+      override def space = self.space
+      override def schema = self.schema
+    }
+
+    override def model = ???
+    override def details = self.details.intersect(other.details)
+
+    override def space = self.space
+    override def schema = self.schema
+  }
 }
 
 object SparkCypherGraph {
