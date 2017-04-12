3f5cb1d4fd6d49f5b0f4c422454686dc153fe5ef
Mats Rydberg <mats@neotechnology.com>
2017-04-12 15:51:03 +0200

    Add rewriter to group label predicates

---
+++ src/main/scala/org/opencypher/spark/impl/parse/sparkCypherRewriting.scala
@@ -0,0 +1,33 @@
+package org.opencypher.spark.impl.parse
+
+import org.neo4j.cypher.internal.frontend.v3_2.{InputPosition, Rewriter, SemanticCheck, bottomUp}
+import org.neo4j.cypher.internal.frontend.v3_2.ast._
+import org.neo4j.cypher.internal.frontend.v3_2.ast.rewriters.StatementRewriter
+import org.neo4j.cypher.internal.frontend.v3_2.phases.{BaseContext, Condition}
+
+object sparkCypherRewriting extends StatementRewriter {
+  override def instance(context: BaseContext): Rewriter = bottomUp(Rewriter.lift {
+    case a@Ands(exprs) =>
+      val (left, right) = exprs.partition {
+        case _: HasLabels => true
+        case _ => false
+      }
+      val singleRight: Expression = right.headOption match {
+        case None => True()(a.position)
+        case Some(expr) => right.tail.headOption match {
+          case None => expr
+          case _ => Ands(right)(a.position)
+        }
+      }
+      RetypingPredicate(left, singleRight)(a.position)
+  })
+
+  override def description: String = "cos specific rewrites"
+
+  override def postConditions: Set[Condition] = Set.empty
+}
+
+case class RetypingPredicate(left: Set[Expression], right: Expression)(val position: InputPosition) extends Expression {
+  override def semanticCheck(ctx: Expression.SemanticContext): SemanticCheck =
+    Ands(left + right)(position).semanticCheck(ctx)
+}
---
+++ src/test/scala/org/opencypher/spark/impl/parse/sparkCypherRewritingTest.scala
@@ -0,0 +1,54 @@
+package org.opencypher.spark.impl.parse
+
+import org.neo4j.cypher.internal.frontend.v3_2.ast._
+import org.opencypher.spark.StdTestSuite
+
+class sparkCypherRewritingTest extends StdTestSuite with AstConstructionTestSupport {
+
+  test("extracts haslabels from ands") {
+    val hasLabels = HasLabels(Variable("n") _, Seq(LabelName("name") _))(pos)
+    val expr = Ands(Set(hasLabels))(pos)
+
+    val result = sparkCypherRewriting.instance(CypherParser.defaultContext)(expr)
+
+    result should equal(RetypingPredicate(Set(hasLabels), True()(pos))(pos))
+  }
+
+  test("extracts only haslabels") {
+    val hasLabels = HasLabels(Variable("n") _, Seq(LabelName("name") _))(pos)
+    val expr = Ands(Set(hasLabels, False() _))(pos)
+
+    val result = sparkCypherRewriting.instance(CypherParser.defaultContext)(expr)
+
+    result should equal(RetypingPredicate(Set(hasLabels), False()(pos))(pos))
+  }
+
+  test("extracts haslabels from inside nested") {
+    val hasLabels = HasLabels(Variable("n") _, Seq(LabelName("name") _))(pos)
+    val expr = Ors(Set(Ands(Set(hasLabels, False() _))(pos), True() _))(pos)
+
+    val result = sparkCypherRewriting.instance(CypherParser.defaultContext)(expr)
+
+    result should equal(Ors(Set(RetypingPredicate(Set(hasLabels), False()(pos))(pos), True()(pos)))(pos))
+  }
+
+  test("extracts all haslabels") {
+    val hasLabels1 = HasLabels(Variable("n") _, Seq(LabelName("name") _))(pos)
+    val hasLabels2 = HasLabels(Variable("m") _, Seq(LabelName("age") _))(pos)
+    val expr = Ands(Set(hasLabels1, False() _, hasLabels2, True() _))(pos)
+
+    val result = sparkCypherRewriting.instance(CypherParser.defaultContext)(expr)
+
+    result should equal(RetypingPredicate(Set(hasLabels1, hasLabels2),
+      Ands(Set(False() _, True() _)) _)(pos))
+  }
+
+  test("doesn't do anything if no haslabels") {
+    val expr = Ands(Set(False() _, True() _))(pos)
+
+    val result = sparkCypherRewriting.instance(CypherParser.defaultContext)(expr)
+
+    result should equal(RetypingPredicate(Set.empty, Ands(Set(False() _, True() _)) _)(pos))
+  }
+
+}
