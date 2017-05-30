465234974573b3ab091363bd5ce33519c79ac90d
Stefan Plantikow <stefan.plantikow@googlemail.com>
2017-05-30 23:43:28 +0200

    Tentative fix to result syntax

--- src/main/scala/org/opencypher/spark/impl/syntax/CypherSyntax.scala
+++ src/main/scala/org/opencypher/spark/impl/syntax/CypherSyntax.scala
@@ -1,7 +1,6 @@
 package org.opencypher.spark.impl.syntax
 
 import org.opencypher.spark.api.graph.{CypherGraph, CypherResult}
-import org.opencypher.spark.api.spark.{SparkCypherGraph, SparkCypherResult}
 import org.opencypher.spark.api.value.CypherValue
 import org.opencypher.spark.impl.classes.Cypher
 
@@ -12,6 +11,21 @@ trait CypherSyntax {
   implicit def cypherEngineGraphSyntax[G <: CypherGraph, C <: Cypher { type Graph = G }](graph: G)(implicit engine: C)
   : CypherGraphOps[G, C] =
     new CypherGraphOps(graph)
+
+
+  implicit def cypherEngineResultSyntax[R <: CypherResult](result: R) : CypherResultOps[R] = new CypherResultOps(result)
+}
+
+final class CypherResultOps[R <: CypherResult](val result: R) extends AnyVal {
+
+  def cypher[C <: Cypher { type Graph = result.Graph; type Result = R }](query: String)(implicit engine: C): R =
+    engine.cypher(result.graph, query)
+
+  def cypher[C <: Cypher { type Graph = result.Graph; type Result = R }](
+    query: String,
+    parameters: Map[String, CypherValue]
+  )(implicit engine: C): R =
+    engine.cypher(result.graph, query)
 }
 
 final class CypherGraphOps[G <: CypherGraph, C <: Cypher { type Graph = G }](graph: G)(implicit val engine: C) {
