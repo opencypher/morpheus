bbce96195d4a38c0162b5c7fa5aabc80f385a624
Stefan Plantikow <stefan.plantikow@googlemail.com>
2017-06-13 11:57:32 +0200

    Refactoring SparkCypherRecords

--- src/main/scala/org/opencypher/spark/impl/instances/spark/SparkCypherRecordsInstances.scala
+++ src/main/scala/org/opencypher/spark/impl/instances/spark/SparkCypherRecordsInstances.scala
@@ -39,106 +39,109 @@ trait SparkCypherRecordsInstances extends Serializable {
     }
   }
 
-  implicit def sparkCypherRecordsTransform(implicit context: RuntimeContext) = new Transform[SparkCypherRecords] with Serializable {
+  implicit def sparkCypherRecordsTransform(implicit context: RuntimeContext) =
+    new Transform[SparkCypherRecords] with Serializable {
 
-    private def liftTernary(f: Row => Option[Boolean]): (Row => Boolean) = {
-      (r: Row) => f(r) match {
-        case None => false
-        case Some(x) => x
-      }
-    }
-
-    override def filter(subject: SparkCypherRecords, expr: Expr, newHeader: RecordHeader): SparkCypherRecords = {
-
-      val filteredRows = asSparkSQLExpr(subject.header, expr, subject.data) match {
-        case Some(sqlExpr) =>
-          subject.data.where(sqlExpr)
-        case None =>
-          val predicate = cypherFilter(newHeader, expr)
-          subject.data.filter(liftTernary(predicate))
+      private def liftTernary(f: Row => Option[Boolean]): (Row => Boolean) = {
+        (r: Row) =>
+          f(r) match {
+            case None => false
+            case Some(x) => x
+          }
       }
 
-      val selectedColumns = newHeader.slots.map { c =>
-        val name = context.columnName(c)
-        filteredRows.col(name)
+      override def filter(subject: SparkCypherRecords, expr: Expr, newHeader: RecordHeader): SparkCypherRecords = {
+
+        val filteredRows = asSparkSQLExpr(subject.header, expr, subject.data) match {
+          case Some(sqlExpr) =>
+            subject.data.where(sqlExpr)
+          case None =>
+            val predicate = cypherFilter(newHeader, expr)
+            subject.data.filter(liftTernary(predicate))
+        }
+
+        val selectedColumns = newHeader.slots.map { c =>
+          val name = context.columnName(c)
+          filteredRows.col(name)
+        }
+
+        val newData = filteredRows.select(selectedColumns: _*)
+
+        SparkCypherRecords.create(newHeader, newData)(subject.session)
       }
 
-      val newData = filteredRows.select(selectedColumns: _*)
+      // TODO: Correctly handle aliasing in the header
+      override def select(subject: SparkCypherRecords, fields: IndexedSeq[Var], newHeader: RecordHeader)
+      : SparkCypherRecords = {
+        val data = subject.data
+        val columns = fields.map { f =>
+          data.col(data.columns(subject.header.slotsFor(f).head.index))
+        }
+        val newData = subject.data.select(columns: _*)
 
-      SparkCypherRecords.create(newHeader, newData)(subject.session)
-    }
-
-    // TODO: Correctly handle aliasing in the header
-    override def select(subject: SparkCypherRecords, fields: IndexedSeq[Var], newHeader: RecordHeader): SparkCypherRecords = {
-
-      val data = subject.data
-      val columns = fields.map { f =>
-        data.col(data.columns(subject.header.slotsFor(f).head.index))
-      }
-      val newData = subject.data.select(columns: _*)
-
-      SparkCypherRecords.create(newHeader, newData)(subject.session)
-    }
-
-    override def reorder(subject: SparkCypherRecords, newHeader: RecordHeader): SparkCypherRecords = {
-      val columns = newHeader.slots.map(context.columnName)
-
-      val newData = subject.data.select(columns.head, columns.tail: _*)
-
-      SparkCypherRecords.create(newHeader, newData)(subject.session)
-    }
-
-    override def alias2(subject: SparkCypherRecords, expr: Expr, v: Var, newHeader: RecordHeader): SparkCypherRecords = {
-      val oldSlot = subject.header.slotsFor(expr).head
-
-      val newSlot = newHeader.slotsFor(v).head
-
-      val oldColumnName = context.columnName(oldSlot)
-      val newColumnName = context.columnName(newSlot)
-
-      val newData = if (subject.data.columns.contains(oldColumnName)) {
-        subject.data.withColumnRenamed(oldColumnName, newColumnName)
-      } else {
-        throw new IllegalStateException(s"Wanted to rename column $oldColumnName, but it was not present!")
+        SparkCypherRecords.create(newHeader, newData)(subject.session)
       }
 
-      SparkCypherRecords.create(newHeader, newData)(subject.session)
-    }
+      override def reorder(subject: SparkCypherRecords, newHeader: RecordHeader): SparkCypherRecords = {
+        val columns = newHeader.slots.map(context.columnName)
 
-    override def project(subject: SparkCypherRecords, expr: Expr, newHeader: RecordHeader): SparkCypherRecords = {
+        val newData = subject.data.select(columns.head, columns.tail: _*)
 
-      val newData = asSparkSQLExpr(newHeader, expr, subject.data) match {
-        case None => throw new NotImplementedError(s"No support for projecting $expr yet")
-
-        case Some(sparkSqlExpr) =>
-          val columnsToSelect = subject.data.columns.map(subject.data.col) :+ sparkSqlExpr
-          subject.data.select(columnsToSelect: _*)
+        SparkCypherRecords.create(newHeader, newData)(subject.session)
       }
 
-      SparkCypherRecords.create(newHeader, newData)(subject.session)
-    }
+      override def alias2(subject: SparkCypherRecords, expr: Expr, v: Var, newHeader: RecordHeader)
+      : SparkCypherRecords = {
+        val oldSlot = subject.header.slotsFor(expr).head
 
-    override def join(lhs: SparkCypherRecords, rhs: SparkCypherRecords)
-                     (lhsSlot: RecordSlot, rhsSlot: RecordSlot): SparkCypherRecords =
-      join(lhs, rhs, lhs.header ++ rhs.header)(lhsSlot, rhsSlot)
+        val newSlot = newHeader.slotsFor(v).head
 
-    override def join(lhs: SparkCypherRecords, rhs: SparkCypherRecords, jointHeader: RecordHeader)
-                     (lhsSlot: RecordSlot, rhsSlot: RecordSlot): SparkCypherRecords = {
+        val oldColumnName = context.columnName(oldSlot)
+        val newColumnName = context.columnName(newSlot)
 
-      if (lhs.session == rhs.session) {
-        val lhsData = lhs.data
-        val rhsData = rhs.data
+        val newData = if (subject.data.columns.contains(oldColumnName)) {
+          subject.data.withColumnRenamed(oldColumnName, newColumnName)
+        } else {
+          throw new IllegalStateException(s"Wanted to rename column $oldColumnName, but it was not present!")
+        }
 
-        val lhsColumn = lhsData.col(lhsData.columns(lhsSlot.index))
-        val rhsColumn = rhsData.col(rhsData.columns(rhsSlot.index))
+        SparkCypherRecords.create(newHeader, newData)(subject.session)
+      }
 
-        val joinExpr: Column = lhsColumn === rhsColumn
-        val jointData = lhsData.join(rhsData, joinExpr, "inner")
+      override def project(subject: SparkCypherRecords, expr: Expr, newHeader: RecordHeader): SparkCypherRecords = {
 
-        SparkCypherRecords.create(jointHeader, jointData)(lhs.session)
-      } else {
-        throw new IllegalArgumentException("Cannot join records from different sessions")
+        val newData = asSparkSQLExpr(newHeader, expr, subject.data) match {
+          case None => throw new NotImplementedError(s"No support for projecting $expr yet")
+
+          case Some(sparkSqlExpr) =>
+            val columnsToSelect = subject.data.columns.map(subject.data.col) :+ sparkSqlExpr
+            subject.data.select(columnsToSelect: _*)
+        }
+
+        SparkCypherRecords.create(newHeader, newData)(subject.session)
+      }
+
+      override def join(lhs: SparkCypherRecords, rhs: SparkCypherRecords)
+                       (lhsSlot: RecordSlot, rhsSlot: RecordSlot): SparkCypherRecords =
+        join(lhs, rhs, lhs.header ++ rhs.header)(lhsSlot, rhsSlot)
+
+      override def join(lhs: SparkCypherRecords, rhs: SparkCypherRecords, jointHeader: RecordHeader)
+                       (lhsSlot: RecordSlot, rhsSlot: RecordSlot): SparkCypherRecords = {
+
+        if (lhs.session == rhs.session) {
+          val lhsData = lhs.data
+          val rhsData = rhs.data
+
+          val lhsColumn = lhsData.col(lhsData.columns(lhsSlot.index))
+          val rhsColumn = rhsData.col(rhsData.columns(rhsSlot.index))
+
+          val joinExpr: Column = lhsColumn === rhsColumn
+          val jointData = lhsData.join(rhsData, joinExpr, "inner")
+
+          SparkCypherRecords.create(jointHeader, jointData)(lhs.session)
+        } else {
+          throw new IllegalArgumentException("Cannot join records from different sessions")
+        }
       }
     }
-  }
 }
