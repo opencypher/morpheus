2296d882db38c0cb33a9553e7b5dec13e75bc81a
Martin Junghanns <m.junghanns@mailbox.org>
2017-07-12 09:36:40 +0200

    Remove Transform trait

--- src/main/scala/org/opencypher/spark/impl/instances/spark/package.scala
+++ src/main/scala/org/opencypher/spark/impl/instances/spark/package.scala
@@ -2,6 +2,5 @@ package org.opencypher.spark.impl.instances
 
 package object spark {
   object all extends spark.AllInstances
-  object records extends SparkCypherRecordsInstances
   object cypher extends SparkCypherInstances
 }
--- src/main/scala/org/opencypher/spark/impl/syntax/package.scala
+++ src/main/scala/org/opencypher/spark/impl/syntax/package.scala
@@ -2,7 +2,6 @@ package org.opencypher.spark.impl
 
 package object syntax {
   object all extends AllSyntax
-  object transform extends TransformSyntax
   object cypher extends CypherSyntax
   object register extends RegisterSyntax
   object header extends RecordHeaderSyntax
