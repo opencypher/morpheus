77cdcadf77ccbebd2bb53caadc2333a6126d7ac6
Stefan Plantikow <stefan.plantikow@googlemail.com>
2017-07-13 14:12:34 +0200

    Simplify graph construction dsl
    Signed-off-by: Stefan Plantikow <stefan.plantikow@googlemail.com>

--- src/test/scala/org/opencypher/spark/api/spark/SparkCypherGraphTest.scala
+++ src/test/scala/org/opencypher/spark/api/spark/SparkCypherGraphTest.scala
@@ -9,31 +9,38 @@ class SparkCypherGraphTest extends SparkCypherTestSuite {
   implicit val space = SparkGraphSpace.empty(session, TokenRegistry.empty)
 
   val `:Person` =
-    NodeScan(EmbeddedNode("n" -> "ID").build
-      .withImpliedLabel("Person")
-      .withOptionalLabel("Swedish" -> "IS_SWEDE")
-      .withProperty("name" -> "NAME")
-      .withProperty("lucky_number" -> "NUM")
-      .verify
-    ).from(SparkCypherRecords.create(session.createDataFrame(Seq(
-      (1, true, "Mats", 23),
-      (2, false, "Martin", 42),
-      (3, false, "Max", 1337),
-      (4, false, "Stefan", 9)
-    )).toDF("ID", "IS_SWEDE", "NAME", "NUM")))
+    NodeScan.on("n" -> "ID") {
+      _.build
+       .withImpliedLabel("Person")
+       .withOptionalLabel("Swedish" -> "IS_SWEDE")
+       .withProperty("name" -> "NAME")
+       .withProperty("lucky_number" -> "NUM")
+    }
+    .from(SparkCypherRecords.create(
+      Seq("ID", "IS_SWEDE", "NAME", "NUM"),
+      Seq(
+        (1, true, "Mats", 23),
+        (2, false, "Martin", 42),
+        (3, false, "Max", 1337),
+        (4, false, "Stefan", 9))
+    ))
 
   val `:KNOWS` =
-    RelationshipScan(EmbeddedRelationship("r" -> "ID").from("SRC").to("DST").relType("KNOWS").build
-      .withProperty("since" -> "SINCE")
-      .verify
-    ).from(SparkCypherRecords.create(session.createDataFrame(Seq(
-      (1, 1, 2, 2017),
-      (1, 2, 3, 2016),
-      (1, 3, 4, 2015),
-      (2, 4, 3, 2016),
-      (2, 5, 4, 2013),
-      (3, 6, 4, 2016)
-    )).toDF("SRC", "ID", "DST", "SINCE")))
+    RelationshipScan.on("r" -> "ID") {
+      _.from("SRC").to("DST").relType("KNOWS")
+       .build
+       .withProperty("since" -> "SINCE")
+    }
+    .from(SparkCypherRecords.create(
+      Seq("SRC", "ID", "DST", "SINCE"),
+      Seq(
+        (1, 1, 2, 2017),
+        (1, 2, 3, 2016),
+        (1, 3, 4, 2015),
+        (2, 4, 3, 2016),
+        (2, 5, 4, 2013),
+        (3, 6, 4, 2016))
+    ))
 
   test("Construct graph from scans") {
      val graph = SparkCypherGraph.create(`:Person`, `:KNOWS`)
