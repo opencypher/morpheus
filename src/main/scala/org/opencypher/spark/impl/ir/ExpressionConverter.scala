bbfe15d8173076a73c1c4781ff48617ad28da7fa
Mats Rydberg <mats@neotechnology.com>
2017-07-28 15:52:47 +0200

    Code style: consistent line-breaking

--- src/main/scala/org/opencypher/spark/impl/ir/ExpressionConverter.scala
+++ src/main/scala/org/opencypher/spark/impl/ir/ExpressionConverter.scala
@@ -34,46 +34,66 @@ final class ExpressionConverter(val globals: GlobalsRegistry) extends AnyVal {
   implicit def toRef(e: ast.Expression): Ref[ast.Expression] = Ref(e)
 
   def convert(e: ast.Expression)(implicit typings: (Ref[ast.Expression]) => CypherType): Expr = e match {
-    case ast.Variable(name) => Var(name)(typings(e))
-    case ast.Parameter(name, _) => Const(constants.constantByName(name))(typings(e))
+    case ast.Variable(name) =>
+      Var(name)(typings(e))
+    case ast.Parameter(name, _) =>
+      Const(constants.constantByName(name))(typings(e))
 
     // Literals
-    case astExpr: ast.IntegerLiteral => IntegerLit(astExpr.value)(typings(e))
-    case ast.StringLiteral(value) => StringLit(value)(typings(e))
-    case _: ast.True => TrueLit()
-    case _: ast.False => FalseLit()
+    case astExpr: ast.IntegerLiteral =>
+      IntegerLit(astExpr.value)(typings(e))
+    case ast.StringLiteral(value) =>
+      StringLit(value)(typings(e))
+    case _: ast.True =>
+      TrueLit()
+    case _: ast.False =>
+      FalseLit()
 
     case ast.Property(m, ast.PropertyKeyName(name)) => Property(convert(m), tokens.propertyKeyByName(name))(typings(e))
 
     // Predicates
-    case ast.Ands(exprs) => new Ands(exprs.map(convert))(typings(e))
-    case ast.Ors(exprs) => new Ors(exprs.map(convert))(typings(e))
+    case ast.Ands(exprs) =>
+      new Ands(exprs.map(convert))(typings(e))
+    case ast.Ors(exprs) =>
+      new Ors(exprs.map(convert))(typings(e))
     case ast.HasLabels(node, labels) =>
       val exprs = labels.map { (l: ast.LabelName) => HasLabel(convert(node), tokens.labelByName(l.name))(typings(e)) }
       if (exprs.size == 1) exprs.head else new Ands(exprs.toSet)(typings(e))
-    case ast.Not(expr) => Not(convert(expr))(typings(e))
-    // MATCH ()-[r]->() WHERE type(r) IN ['FOO', 'BAR] ==> MATCH ()-[r]->() WHERE r[:FOO|BAR]
+    case ast.Not(expr) =>
+      Not(convert(expr))(typings(e))
+      // TODO: Does this belong here still?
     case ast.Equals(f: ast.FunctionInvocation, s: ast.StringLiteral) if f.function == functions.Type =>
       HasType(convert(f.args.head), tokens.relTypeByName(s.value))(CTBoolean)
-    case ast.Equals(lhs, rhs) => Equals(convert(lhs), convert(rhs))(typings(e))
-    case ast.LessThan(lhs, rhs) => LessThan(convert(lhs), convert(rhs))(typings(e))
-    case ast.LessThanOrEqual(lhs, rhs) => LessThanOrEqual(convert(lhs), convert(rhs))(typings(e))
-    case ast.GreaterThan(lhs, rhs) => GreaterThan(convert(lhs), convert(rhs))(typings(e))
-    case ast.GreaterThanOrEqual(lhs, rhs) => GreaterThanOrEqual(convert(lhs), convert(rhs))(typings(e))
+    case ast.Equals(lhs, rhs) =>
+      Equals(convert(lhs), convert(rhs))(typings(e))
+    case ast.LessThan(lhs, rhs) =>
+      LessThan(convert(lhs), convert(rhs))(typings(e))
+    case ast.LessThanOrEqual(lhs, rhs) =>
+      LessThanOrEqual(convert(lhs), convert(rhs))(typings(e))
+    case ast.GreaterThan(lhs, rhs) =>
+      GreaterThan(convert(lhs), convert(rhs))(typings(e))
+    case ast.GreaterThanOrEqual(lhs, rhs) =>
+      GreaterThanOrEqual(convert(lhs), convert(rhs))(typings(e))
     case ast.In(lhs, ast.ListLiteral(elems)) if elems.size == 1 =>
       Equals(convert(lhs), convert(elems.head))(typings(e))
     case RetypingPredicate(lhs, rhs) =>
       new Ands(lhs.map(convert) + convert(rhs))(typings(e))
 
     // Arithmetics
-    case ast.Add(lhs, rhs) => Add(convert(lhs), convert(rhs))(typings(e))
-    case ast.Subtract(lhs, rhs) => Subtract(convert(lhs), convert(rhs))(typings(e))
-    case ast.Multiply(lhs, rhs) => Multiply(convert(lhs), convert(rhs))(typings(e))
-    case ast.Divide(lhs, rhs) => Divide(convert(lhs), convert(rhs))(typings(e))
+    case ast.Add(lhs, rhs) =>
+      Add(convert(lhs), convert(rhs))(typings(e))
+    case ast.Subtract(lhs, rhs) =>
+      Subtract(convert(lhs), convert(rhs))(typings(e))
+    case ast.Multiply(lhs, rhs) =>
+      Multiply(convert(lhs), convert(rhs))(typings(e))
+    case ast.Divide(lhs, rhs) =>
+      Divide(convert(lhs), convert(rhs))(typings(e))
 
     // Functions
-    case funcInv:ast.FunctionInvocation => funcInv.toCAPSFunction(funcInv.args.map(convert), typings(e))
+    case funcInv:ast.FunctionInvocation =>
+      funcInv.toCAPSFunction(funcInv.args.map(convert), typings(e))
 
-    case _ => throw new NotImplementedError(s"Not yet able to convert expression: $e")
+    case _ =>
+      throw new NotImplementedError(s"Not yet able to convert expression: $e")
   }
 }
