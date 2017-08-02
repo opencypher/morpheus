8509213d7d85cd0ae1ff57edb2169faed287b919
DarthMax <max.kiessling@neo4j.com>
2017-08-02 15:48:34 +0200

    Adopt CsvGraphLoader in CSVDemo

--- src/main/scala/org/opencypher/spark/CSVDemo.scala
+++ src/main/scala/org/opencypher/spark/CSVDemo.scala
@@ -5,10 +5,9 @@ import java.util.Calendar
 import org.apache.spark.SparkConf
 import org.apache.spark.serializer.KryoSerializer
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.types._
+import org.opencypher.spark.api.io.CsvGraphLoader
 import org.opencypher.spark.api.ir.global.TokenRegistry
-import org.opencypher.spark.api.record.{NodeScan, RelationshipScan}
-import org.opencypher.spark.api.spark.{SparkCypherGraph, SparkCypherRecords, SparkCypherResult, SparkGraphSpace}
+import org.opencypher.spark.api.spark.{SparkCypherGraph, SparkCypherResult, SparkGraphSpace}
 import org.opencypher.spark.impl.instances.spark.cypher._
 import org.opencypher.spark.impl.syntax.cypher._
 import org.opencypher.spark_legacy.CypherKryoRegistrar
@@ -28,158 +27,7 @@ object CSVDemo {
 
   implicit val graphSpace = SparkGraphSpace.empty(session, TokenRegistry.empty)
 
-  lazy val graph = {
-
-    // (1) Load node CSV files (by label)
-    val `:Person` =
-      NodeScan.on("n0" -> "ID") {
-        _.build
-          .withImpliedLabel("Person")
-          .withOptionalLabel("Male" -> "IS_MALE")
-          .withOptionalLabel("Female" -> "IS_FEMALE")
-          .withPropertyKey("firstName" -> "FIRST_NAME")
-          .withPropertyKey("lastName" -> "LAST_NAME")
-          .withPropertyKey("createdAt" -> "CREATED_AT")
-      }.from(loadCSV("/demo/ldbc_1/nodes/person.csv.gz", StructType(Array(
-        StructField("ID", LongType, false),
-        StructField("FIRST_NAME", StringType, false),
-        StructField("LAST_NAME", StringType, false),
-        StructField("IS_MALE", BooleanType, false),
-        StructField("IS_FEMALE", BooleanType, false),
-        StructField("CREATED_AT", LongType, false)
-      ))))
-
-    val `:University` =
-      NodeScan.on("n1" -> "ID") {
-        _.build
-          .withImpliedLabel("University")
-          .withPropertyKey("name" -> "NAME")
-      }.from(loadCSV("/demo/ldbc_1/nodes/university.csv.gz", StructType(Array(
-        StructField("ID", LongType, false),
-        StructField("NAME", StringType, false)
-      ))))
-
-    val `:Company` =
-      NodeScan.on("n2" -> "ID") {
-        _.build
-          .withImpliedLabel("Company")
-          .withPropertyKey("name" -> "NAME")
-      }.from(loadCSV("/demo/ldbc_1/nodes/company.csv.gz", StructType(Array(
-        StructField("ID", LongType, false),
-        StructField("NAME", StringType, false)
-      ))))
-
-    val `:City` =
-      NodeScan.on("n3" -> "ID") {
-        _.build
-          .withImpliedLabel("City")
-          .withPropertyKey("name" -> "NAME")
-      }.from(loadCSV("/demo/ldbc_1/nodes/city.csv.gz", StructType(Array(
-        StructField("ID", LongType, false),
-        StructField("NAME", StringType, false)
-      ))))
-
-    val `:Country` =
-      NodeScan.on("n4" -> "ID") {
-        _.build
-          .withImpliedLabel("Country")
-          .withPropertyKey("name" -> "NAME")
-      }.from(loadCSV("/demo/ldbc_1/nodes/country.csv.gz", StructType(Array(
-        StructField("ID", LongType, false),
-        StructField("NAME", StringType, false)
-      ))))
-
-    val `:Continent` =
-      NodeScan.on("n5" -> "ID") {
-        _.build
-          .withImpliedLabel("Continent")
-          .withPropertyKey("name" -> "NAME")
-      }.from(loadCSV("/demo/ldbc_1/nodes/continent.csv.gz", StructType(Array(
-        StructField("ID", LongType, false),
-        StructField("NAME", StringType, false)
-      ))))
-
-    val `:Tag` =
-      NodeScan.on("n6" -> "ID") {
-        _.build
-          .withImpliedLabel("Tag")
-          .withPropertyKey("name" -> "NAME")
-      }.from(loadCSV("/demo/ldbc_1/nodes/tag.csv.gz", StructType(Array(
-        StructField("ID", LongType, false),
-        StructField("NAME", StringType, false)
-      ))))
-
-    // (2) Load edge CSV files (by type)
-    val `:KNOWS` =
-      RelationshipScan.on("e0" -> "ID") {
-        _.from("SRC").to("DST").relType("KNOWS")
-          .build
-      }.from(loadCSV("/demo/ldbc_1/rels/knows.csv.gz", StructType(Array(
-        StructField("SRC", LongType, false),
-        StructField("ID", LongType, false),
-        StructField("DST", LongType, false)
-      ))))
-
-    val `:STUDY_AT` =
-      RelationshipScan.on("e1" -> "ID") {
-        _.from("SRC").to("DST").relType("STUDY_AT")
-          .build
-          .withPropertyKey("classYear" -> "CLASS_YEAR")
-      }.from(loadCSV("/demo/ldbc_1/rels/studyAt.csv.gz", StructType(Array(
-        StructField("SRC", LongType, false),
-        StructField("ID", LongType, false),
-        StructField("DST", LongType, false),
-        StructField("CLASS_YEAR", IntegerType, false)
-      ))))
-
-    val `:WORK_AT` =
-      RelationshipScan.on("e2" -> "ID") {
-        _.from("SRC").to("DST").relType("WORK_AT")
-          .build
-          .withPropertyKey("workFrom" -> "WORK_FROM")
-      }.from(loadCSV("/demo/ldbc_1/rels/workAt.csv.gz", StructType(Array(
-        StructField("SRC", LongType, false),
-        StructField("ID", LongType, false),
-        StructField("DST", LongType, false),
-        StructField("WORK_FROM", IntegerType, false)
-      ))))
-
-    val `:IS_LOCATED_IN` =
-      RelationshipScan.on("e3" -> "ID") {
-        _.from("SRC").to("DST").relType("IS_LOCATED_IN")
-          .build
-      }.from(loadCSV("/demo/ldbc_1/rels/isLocatedIn.csv.gz", StructType(Array(
-        StructField("SRC", LongType, false),
-        StructField("ID", LongType, false),
-        StructField("DST", LongType, false)
-      ))))
-
-    val `:IS_PART_OF` =
-      RelationshipScan.on("e4" -> "ID") {
-        _.from("SRC").to("DST").relType("IS_PART_OF")
-          .build
-      }.from(loadCSV("/demo/ldbc_1/rels/isPartOf.csv.gz", StructType(Array(
-        StructField("SRC", LongType, false),
-        StructField("ID", LongType, false),
-        StructField("DST", LongType, false)
-      ))))
-
-    val `:HAS_INTEREST` =
-      RelationshipScan.on("e5" -> "ID") {
-        _.from("SRC").to("DST").relType("HAS_INTEREST")
-          .build
-      }.from(loadCSV("/demo/ldbc_1/rels/hasInterest.csv.gz", StructType(Array(
-        StructField("SRC", LongType, false),
-        StructField("ID", LongType, false),
-        StructField("DST", LongType, false)
-      ))))
-
-    SparkCypherGraph.create(
-      // node scans
-      `:Person`, `:University`, `:Company`, `:City`, `:Country`, `:Continent`, `:Tag`,
-      // rel scans
-      `:KNOWS`, `:STUDY_AT`, `:WORK_AT`, `:IS_LOCATED_IN`, `:IS_PART_OF`, `:HAS_INTEREST`)
-  }
+  lazy val graph: SparkCypherGraph = new CsvGraphLoader(getClass.getResource("/demo/ldbc_1").getFile).load
 
   session.sparkContext.setLogLevel(Logging.get())
 
@@ -194,22 +42,5 @@ object CSVDemo {
     println(s"Returned ${result.records.toDF().count()} row(s) in ${System.currentTimeMillis() - start} ms")
 
     result
-  }
-
-  def loadCSV(file: String)(implicit sc: SparkSession): SparkCypherRecords = {
-    val df = sc.read
-      .option("header", "true")
-      .option("inferSchema", "true")
-      .csv(getClass.getResource(file).getFile)
-
-    SparkCypherRecords.create(df)
-  }
-
-  def loadCSV(file: String, schema: StructType)(implicit sc: SparkSession): SparkCypherRecords = {
-    val df = sc.read
-      .schema(schema)
-      .csv(getClass().getResource(file).getFile)
-
-    SparkCypherRecords.create(df)
   }
 }
