8e62482c177a3393f747057fb2af8e0b57d95508
Philip Stutz <philip.stutz@gmail.com>
2017-09-13 16:13:20 +0100

    Fix unused import

--- src/test/scala/org/opencypher/caps/impl/spark/cypher/ReturnAcceptanceTest.scala
+++ src/test/scala/org/opencypher/caps/impl/spark/cypher/ReturnAcceptanceTest.scala
@@ -15,7 +15,7 @@
  */
 package org.opencypher.caps.impl.spark.cypher
 
-import org.opencypher.caps.api.value.{CypherList, CypherMap}
+import org.opencypher.caps.api.value.CypherMap
 import org.opencypher.caps.test.CAPSTestSuite
 
 import scala.collection.Bag
