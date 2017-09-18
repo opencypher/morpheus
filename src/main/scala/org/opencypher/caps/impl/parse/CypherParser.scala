49a99f240b47f82cbce626dcdb41d805cd57f11e
Mats Rydberg <mats@neotechnology.com>
2017-09-18 13:57:45 +0200

    Work around variable re-use bug in frontend

--- src/main/scala/org/opencypher/caps/impl/parse/CypherParser.scala
+++ src/main/scala/org/opencypher/caps/impl/parse/CypherParser.scala
@@ -56,6 +56,7 @@ trait CypherParser extends CompilationStage[String, Statement, BaseContext] {
       SyntaxDeprecationWarnings andThen
       PreparatoryRewriting andThen
       SemanticAnalysis(warn = true, SemanticFeature.MultipleGraphs, SemanticFeature.WithInitialQuerySignature).adds(BaseContains[SemanticState]) andThen
+      fixReferenceEqualityBugInFrontend andThen
       AstRewriting(RewriterStepSequencer.newPlain, Forced) andThen
       SemanticAnalysis(warn = false, SemanticFeature.MultipleGraphs, SemanticFeature.WithInitialQuerySignature) andThen
       Namespacer andThen
