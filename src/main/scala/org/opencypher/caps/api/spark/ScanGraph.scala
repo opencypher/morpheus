f89e8d66c4d918fdd98aeeb44ef52feb6223d816
Philip Stutz <philip.stutz@gmail.com>
2017-09-29 10:44:44 +0100

    Add license headers
    - Remove unused imports

--- src/main/scala/org/opencypher/caps/api/spark/ScanGraph.scala
+++ src/main/scala/org/opencypher/caps/api/spark/ScanGraph.scala
@@ -1,30 +1,27 @@
 /**
-  * Copyright (c) 2016-2017 "Neo4j, Inc." [https://neo4j.com]
-  *
-  * Licensed under the Apache License, Version 2.0 (the "License");
-  * you may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at
-  *
-  * http://www.apache.org/licenses/LICENSE-2.0
-  *
-  * Unless required by applicable law or agreed to in writing, software
-  * distributed under the License is distributed on an "AS IS" BASIS,
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  */
+ * Copyright (c) 2016-2017 "Neo4j, Inc." [https://neo4j.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.opencypher.caps.api.spark
 
 import cats.data.NonEmptyVector
-import org.apache.spark.sql.Row
 import org.opencypher.caps.api.expr._
 import org.opencypher.caps.api.record._
 import org.opencypher.caps.api.schema.Schema
 import org.opencypher.caps.api.types.{CTNode, CTRelationship, CypherType, DefiniteCypherType}
 import org.opencypher.caps.impl.record.CAPSRecordsTokens
-import org.opencypher.caps.impl.spark.SparkColumnName
 import org.opencypher.caps.impl.spark.exception.Raise
-import org.opencypher.caps.ir.api.global.Label
 
 class ScanGraph(val scans: Seq[GraphScan], val schema: Schema, override val tokens: CAPSRecordsTokens)
   (implicit val session: CAPSSession) extends CAPSGraph {
