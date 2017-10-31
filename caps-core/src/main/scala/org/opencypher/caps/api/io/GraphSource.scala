8e13301a4575bb81f2f8ebf978b1e27479c8903a
pstutz <philip@stutz.tech>
2017-10-31 23:53:55 +0000

    Fix license in GraphSource

--- caps-core/src/main/scala/org/opencypher/caps/api/io/GraphSource.scala
+++ caps-core/src/main/scala/org/opencypher/caps/api/io/GraphSource.scala
@@ -1,19 +1,4 @@
 /**
- * Copyright (c) 2016-2017 "Neo4j, Inc." [https://neo4j.com]
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/**
   * Copyright (c) 2016-2017 "Neo4j, Inc." [https://neo4j.com]
   *
   * Licensed under the Apache License, Version 2.0 (the "License");
@@ -43,7 +28,7 @@ trait GraphSource {
   self =>
 
   type Session <: CypherSession { type Session = self.Session; type Graph = self.Graph }
-  type Graph <: CypherGraph { type Session = self.Session; type Graph = self.Graph }
+  type Graph <: CypherGraph { type Session     = self.Session; type Graph = self.Graph }
 
   /**
     * The session tied to this graph source.
