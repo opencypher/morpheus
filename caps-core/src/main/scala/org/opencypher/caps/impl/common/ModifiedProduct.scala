f43d2f348878ed5003e23e8441893422dc63c8d1
pstutz <philip@stutz.tech>
2017-11-17 14:53:09 +0000

    Fix style for variables in comments

--- caps-core/src/main/scala/org/opencypher/caps/impl/common/ModifiedProduct.scala
+++ caps-core/src/main/scala/org/opencypher/caps/impl/common/ModifiedProduct.scala
@@ -18,7 +18,7 @@ package org.opencypher.caps.impl.common
 import scala.util.hashing.MurmurHash3
 
 /**
-  * Allows to exclude constructor parameters from inclusion in the ```hashCode``` and ```equals```
+  * Allows to exclude constructor parameters from inclusion in the `hashCode` and `equals`
   * methods of a product.
   */
 abstract class ModifiedProduct extends Product {
