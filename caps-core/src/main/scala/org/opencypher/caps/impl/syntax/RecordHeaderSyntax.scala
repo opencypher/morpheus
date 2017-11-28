12cb05bc3b9b96feda3ce0416cf26fce9750004b
Mats Rydberg <mats@neotechnology.com>
2017-11-28 14:41:05 +0100

    Remove space in import

--- caps-core/src/main/scala/org/opencypher/caps/impl/syntax/RecordHeaderSyntax.scala
+++ caps-core/src/main/scala/org/opencypher/caps/impl/syntax/RecordHeaderSyntax.scala
@@ -18,7 +18,7 @@ package org.opencypher.caps.impl.syntax
 import cats.data.State
 import cats.data.State.{get, set}
 import org.opencypher.caps.api.record._
-import org.opencypher.caps.impl. record.{AdditiveUpdateResult, InternalHeader, RemovingUpdateResult}
+import org.opencypher.caps.impl.record.{AdditiveUpdateResult, InternalHeader, RemovingUpdateResult}
 
 import scala.language.implicitConversions
 
