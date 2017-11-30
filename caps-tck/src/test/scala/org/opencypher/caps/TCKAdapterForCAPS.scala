d37be821671ad429bceefce0fdc3780a81525b75
Mats Rydberg <mats@neotechnology.com>
2017-11-30 12:13:33 +0100

    Prototype new TCK API implementation

---
+++ caps-tck/src/test/scala/org/opencypher/caps/TCKAdapterForCAPS.scala
@@ -0,0 +1,41 @@
+package org.opencypher.caps
+
+import org.opencypher.caps.api.spark.{CAPSGraph, CAPSRecords}
+import org.opencypher.caps.api.value.{CypherValue => CAPSValue}
+import org.opencypher.tools.tck.api._
+import org.opencypher.tools.tck.values.CypherValue
+
+object TCKAdapterForCAPS {
+
+  implicit class AsTckGraph(graph: CAPSGraph) extends Graph {
+
+    override def execute(query: String, params: Map[String, CypherValue], queryType: QueryType): (Graph, Records) = {
+
+      queryType match {
+        case InitQuery =>
+          // we don't support any updates at all
+          // redirect to GDL or neo4j-harness or w/e
+          this -> CypherValueRecords.empty
+        case SideEffectQuery =>
+          // this one is tricky, not sure how can do it without Cypher
+          this -> CypherValueRecords.empty
+        case ExecQuery =>
+          // mapValues is lazy, so we force it for debug purposes
+          val capsResult = graph.cypher(query, params.mapValues(CAPSValue(_)).view.force)
+          val tckRecords: Records = convertToTckRecords(capsResult.records)
+          val tckGraph: Graph = capsResult.graphs.values.headOption.map(AsTckGraph).getOrElse(this)
+          (tckGraph, tckRecords)
+      }
+    }
+
+    private def convertToTckRecords(records: CAPSRecords): StringRecords = {
+      val header = records.header.fieldsInOrder.map(_.name).toList
+      val rows = records.toLocalScalaIterator.map { cypherMap =>
+        cypherMap.keys.map(k => k -> cypherMap.get(k).orNull.toString).toMap
+      }.toList
+
+      StringRecords(header, rows)
+    }
+  }
+
+}
