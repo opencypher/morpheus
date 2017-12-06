74db7eba8d93021f427f5049708641528260699a
DarthMax <max.kiessling@neo4j.com>
2017-12-06 11:03:56 +0100

    Add Return clause to rewritten pattern expression

--- caps-core/src/main/scala/org/opencypher/caps/impl/parse/rewriter/extractSubqueryFromPatternExpression.scala
+++ caps-core/src/main/scala/org/opencypher/caps/impl/parse/rewriter/extractSubqueryFromPatternExpression.scala
@@ -2,6 +2,7 @@ package org.opencypher.caps.impl.parse.rewriter
 
 import org.neo4j.cypher.internal.frontend.v3_3.ast.rewriters.{nameMatchPatternElements, normalizeMatchPredicates}
 import org.neo4j.cypher.internal.frontend.v3_3.ast.{ASTNode, _}
+import org.neo4j.cypher.internal.frontend.v3_3.helpers.{FreshIdNameGenerator, UnNamedNameGenerator}
 import org.neo4j.cypher.internal.frontend.v3_3.{CypherException, InputPosition, Rewriter, SemanticCheck, SemanticCheckResult, topDown}
 
 case class extractSubqueryFromPatternExpression(mkException: (String, InputPosition) => CypherException) extends Rewriter {
@@ -28,11 +29,28 @@ case class extractSubqueryFromPatternExpression(mkException: (String, InputPosit
     case p@PatternExpression(relationshipsPattern) =>
       val patternPosition: InputPosition = p.position
       val newPattern = Pattern(Seq(EveryPath(relationshipsPattern.element)))(patternPosition)
-      Exists(
-        Match(optional = false, newPattern, Seq.empty, None)(patternPosition)
-            .endoRewrite(nameMatchPatternElements)
-            .endoRewrite(normalizeMatchPredicates))(patternPosition)
 
+      val joinVariables = relationshipsPattern.element.treeFold(Seq.empty[Variable]) {
+        case NodePattern(Some(v), _, _) => (acc) => (acc :+ v, None)
+        case RelationshipPattern(Some(v), _, _, _, _, _) => (acc) => (acc :+ v, None)
+      }
+
+      val returnItems = joinVariables.map(v => AliasedReturnItem(v, v)(v.position))
+
+      val trueVariable = Variable(UnNamedNameGenerator.name(p.position))(p.position)
+      val returnItemsWithTrue = returnItems :+ AliasedReturnItem(True()(trueVariable.position), trueVariable)(trueVariable.position)
+
+      Exists(
+        SingleQuery(
+          Seq(
+            Match(optional = false, newPattern, Seq.empty, None)(patternPosition)
+                .endoRewrite(nameMatchPatternElements)
+                .endoRewrite(normalizeMatchPredicates)
+            ,
+            Return(ReturnItems(includeExisting = false, returnItemsWithTrue)(patternPosition), None)(patternPosition)
+          )
+        )(patternPosition)
+      )(patternPosition)
 
     case a@And(lhs, rhs) =>
       And(lhs.endoRewrite(whereRewriter), rhs.endoRewrite(whereRewriter))(a.position)
