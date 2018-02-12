8688cba2dab356c90f738c89f6adb0efa6be7b41
pstutz <philip@stutz.tech>
2018-02-12 22:25:03 +0000

    Add TCK compatible CypherException to API package

--- caps-api/src/main/scala/org/opencypher/caps/api/exception/CypherException.scala
+++ caps-api/src/main/scala/org/opencypher/caps/api/exception/CypherException.scala
@@ -15,17 +15,106 @@
  */
 package org.opencypher.caps.api.exception
 
-abstract class CypherException(msg: String) extends RuntimeException(msg) with Serializable
+import org.opencypher.caps.api.exception.CypherException.{ErrorDetails, ErrorPhase, ErrorType}
 
-final case class SchemaException(msg: String) extends CypherException(msg)
+/**
+  * Cypher exceptions are thrown due to the reasons specified by the Cypher TCK and follow the specified format.
+  */
+abstract class CypherException(errorType: ErrorType, phase: ErrorPhase, detail: ErrorDetails) extends RuntimeException(s"$errorType during $phase. Details: $detail") with Serializable
 
-final case class CypherValueException(msg: String) extends CypherException(msg)
+object CypherException {
 
-final case class NotImplementedException(msg: String) extends CypherException(msg)
+  sealed trait ErrorType
 
-final case class IllegalStateException(msg: String) extends CypherException(msg)
+  /**
+    * Possible error types as specified by the TCK.
+    */
+  object ErrorType {
 
-final case class IllegalArgumentException(expected: Any, actual: Any = "none")
-  extends CypherException(s"\nExpected:\n\t$expected\nFound:\n\t$actual")
+    object SyntaxError extends ErrorType
 
-final case class UnsupportedOperationException(msg: String) extends CypherException(msg)
+    object ParameterMissing extends ErrorType
+
+    object ConstraintVerificationFailed extends ErrorType
+
+    //TODO: Validation/verification are too similar. Fix in TCK?
+    object ConstraintValidationFailed extends ErrorType
+
+    object EntityNotFound extends ErrorType
+
+    object PropertyNotFound extends ErrorType
+
+    object LabelNotFound extends ErrorType
+
+    object TypeError extends ErrorType
+
+    object ArgumentError extends ErrorType
+
+    object ArithmeticError extends ErrorType
+
+    object ProcedureError extends ErrorType
+
+  }
+
+  sealed trait ErrorPhase
+
+  /**
+    * Possible error phases as specified by the TCK.
+    */
+  object ErrorPhase {
+
+    object Runtime extends ErrorPhase
+
+    object CompileTime extends ErrorPhase
+
+  }
+
+  sealed trait ErrorDetails
+
+  /**
+    * Possible error details as specified by the TCK.
+    */
+  object ErrorDetails {
+    object InvalidElementAccess extends ErrorDetails
+    object MapElementAccessByNonString extends ErrorDetails
+    object ListElementAccessByNonInteger extends ErrorDetails
+    object NestedAggregation extends ErrorDetails
+    object NegativeIntegerArgument extends ErrorDetails
+    object DeleteConnectedNode extends ErrorDetails
+    object RequiresDirectedRelationship extends ErrorDetails
+    object InvalidRelationshipPattern extends ErrorDetails
+    object VariableAlreadyBound extends ErrorDetails
+    object InvalidArgumentType extends ErrorDetails
+    object InvalidArgumentValue extends ErrorDetails
+    object NumberOutOfRange extends ErrorDetails
+    object UndefinedVariable extends ErrorDetails
+    object VariableTypeConflict extends ErrorDetails
+    object RelationshipUniquenessViolation extends ErrorDetails
+    object CreatingVarLength extends ErrorDetails
+    object InvalidParameterUse extends ErrorDetails
+    object InvalidClauseComposition extends ErrorDetails
+    object FloatingPointOverflow extends ErrorDetails
+    object PropertyAccessOnNonMap extends ErrorDetails
+    object InvalidArgumentExpression extends ErrorDetails
+    object InvalidUnicodeCharacter extends ErrorDetails
+    object NonConstantExpression extends ErrorDetails
+    object NoSingleRelationshipType extends ErrorDetails
+    object InvalidAggregation extends ErrorDetails
+    object UnknownFunction extends ErrorDetails
+    object InvalidNumberLiteral extends ErrorDetails
+    object InvalidUnicodeLiteral extends ErrorDetails
+    object MergeReadOwnWrites extends ErrorDetails
+    object NoExpressionAlias extends ErrorDetails
+    object DifferentColumnsInUnion extends ErrorDetails
+    object InvalidDelete extends ErrorDetails
+    object InvalidPropertyType extends ErrorDetails
+    object ColumnNameConflict extends ErrorDetails
+    object NoVariablesInScope extends ErrorDetails
+    object DeletedEntityAccess extends ErrorDetails
+    object InvalidArgumentPassingMode extends ErrorDetails
+    object InvalidNumberOfArguments extends ErrorDetails
+    object MissingParameter extends ErrorDetails
+    object ProcedureNotFound extends ErrorDetails
+  }
+
+}
