206084bc035c0340817a0abf660744080f97dd09
DarthMax <max.kiessling@neo4j.com>
2018-02-26 18:33:58 +0100

    Rename modules

--- caps-spark/src/main/scala/org/opencypher/spark/impl/AllInstances.scala
+++
@@ -1,5 +0,0 @@
-package org.opencypher.spark.impl
-
-import org.opencypher.okapi.ir.impl.refactor.instances.ExprBlockInstances
-
-trait AllInstances extends ExprBlockInstances
--- caps-spark/src/main/scala/org/opencypher/spark/impl/impl.scala
+++
@@ -1,5 +0,0 @@
-package org.opencypher.spark
-
-package object impl {
-  object all extends org.opencypher.spark.impl.AllInstances
-}
