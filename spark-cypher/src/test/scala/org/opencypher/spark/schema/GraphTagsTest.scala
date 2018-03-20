17d7b79bf1c2fa7412e8112eb197962fcb88911f
Martin Junghanns <martin.junghanns@neotechnology.com>
2018-03-20 12:30:48 +0100

    Implement tag replacement
    Co-authored-by: Philip Stutz <philip.stutz@gmail.com>

--- spark-cypher/src/test/scala/org/opencypher/spark/schema/GraphTagsTest.scala
+++ spark-cypher/src/test/scala/org/opencypher/spark/schema/GraphTagsTest.scala
@@ -41,10 +41,12 @@ class GraphTagsTest extends CAPSTestSuite with Matchers {
   val idBits = 50
 
   val tagMask: Long = -1L << idBits
+  val invertedTagMask: Long = ~tagMask
+  val invertedTagMaskLit: Column = functions.lit(invertedTagMask)
 
   def setTag(nodeId: Long, tag: Long): Long = {
     require((nodeId & tagMask) == 0L)
-    val r = nodeId | (tag << idBits)
+    val r = (nodeId & invertedTagMask) | (tag << idBits)
     println(s"setTag(nodeId=$nodeId, tag=$tag)=$r")
     r
   }
@@ -57,29 +59,73 @@ class GraphTagsTest extends CAPSTestSuite with Matchers {
 
   implicit class Tagging(val nodeId: Column) {
 
+    def replaceTag(from: Long, to: Long): Column = functions
+      .when(getTag === functions.lit(from), setTag(to))
+      .otherwise(nodeId)
+
     def setTag(tag: Long): Column = {
       val tagLit = functions.lit(tag << idBits)
-      val newId = nodeId.bitwiseOR(tagLit)
+      val newId = nodeId
+        .bitwiseAND(invertedTagMaskLit)
+        .bitwiseOR(tagLit)
       newId
     }
 
-    def getTag: Column = {
-      val tag = functions.shiftRight(nodeId, idBits)
-      tag
-    }
+    def getTag: Column = functions.shiftRight(nodeId, idBits)
+
   }
 
   it("successfully sets tags") {
     val tag = 1L
 
-    val df: Dataset[lang.Long] = sparkSession.range(2)
+    val df: Dataset[lang.Long] = sparkSession.range(10)
     val ids = df.col("id")
     val tagged = ids.setTag(tag)
-    val extracted = tagged.getTag
     val taggedDf = df.withColumn("tagged", tagged)
 
+    val extracted = tagged.getTag
     val extractedTagDf = taggedDf.withColumn("extracted", extracted)
     extractedTagDf.select("extracted").collect().forall(row => row.get(0) == tag) shouldBe true
+  }
+
+  it("should overwrite tags") {
+    val tag1 = 1L
+    val tag2 = 2L
+
+    val df = sparkSession.range(10)
+    val ids = df.col("id")
+    val tagged1 = ids.setTag(tag1)
+    val tagged1Df = df.withColumn("tagged1", tagged1)
+    val taggedIds = tagged1Df.col("tagged1")
+
+    val tagged2 = taggedIds.setTag(tag2)
+    val tagged2Df = tagged1Df.withColumn("tagged2", tagged2)
+
+    val extracted = tagged2.getTag
+    val extractedTagDf = tagged2Df.withColumn("extracted", extracted)
+
+    extractedTagDf.show()
+    extractedTagDf.select("extracted").collect().forall(row => row.get(0) == tag2) shouldBe true
+  }
+
+  it("should replace tag with other tag") {
+    val tag1 = 1L
+    val tag2 = 2L
+    val tag3 = 3L
+
+    val df1 = sparkSession.range(2)
+    val taggedDf1 = df1.withColumn("id", df1.col("id").setTag(tag1))
+
+    val df2 = sparkSession.range(2)
+    val taggedDf2 = df2.withColumn("id", df2.col("id").setTag(tag2))
+
+    val unionDf = taggedDf1.union(taggedDf2)
+
+    val replacedDf = unionDf.withColumn("id", unionDf.col("id").replaceTag(tag1, tag3))
+
+    val idRows = replacedDf.select(replacedDf.col("id").getTag).collect()
+
+    idRows.toList.map(_.get(0).asInstanceOf[Long]).sorted shouldBe List(2, 2, 3, 3)
   }
 
 }
