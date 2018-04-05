b35be45f878a8f821e4c578bebf0c8f1aa76dee7
pstutz <philip.stutz@gmail.com>
2018-04-05 18:08:38 +0200

    Run parameterised tests individually

--- spark-cypher/src/test/scala/org/opencypher/spark/impl/acceptance/ExpandIntoBehaviour.scala
+++ spark-cypher/src/test/scala/org/opencypher/spark/impl/acceptance/ExpandIntoBehaviour.scala
@@ -27,123 +27,121 @@
 package org.opencypher.spark.impl.acceptance
 
 import org.opencypher.okapi.api.value.CypherValue.CypherMap
-import org.opencypher.spark.impl.CAPSGraph
-
 import org.opencypher.okapi.ir.test.support.Bag
+import org.opencypher.spark.test.CAPSTestSuite
+import org.scalatest.DoNotDiscover
 
-trait ExpandIntoBehaviour {
-  self: AcceptanceTest =>
+@DoNotDiscover
+class ExpandIntoBehaviour extends CAPSTestSuite with DefaultGraphInit {
 
-  def expandIntoBehaviour(initGraph: String => CAPSGraph): Unit = {
-    test("test expand into for dangling edge") {
-      // Given
-      val given = initGraph(
-        """
-          |CREATE (p1:Person {name: "Alice"})
-          |CREATE (p2:Person {name: "Bob"})
-          |CREATE (p3:Person {name: "Eve"})
-          |CREATE (p4:Person {name: "Carl"})
-          |CREATE (p5:Person {name: "Richard"})
-          |CREATE (p1)-[:KNOWS]->(p2)
-          |CREATE (p2)-[:KNOWS]->(p3)
-          |CREATE (p1)-[:KNOWS]->(p3)
-          |CREATE (p3)-[:KNOWS]->(p4)
-          |CREATE (p3)-[:KNOWS]->(p5)
-        """.stripMargin)
+  test("test expand into for dangling edge") {
+    // Given
+    val given = initGraph(
+      """
+        |CREATE (p1:Person {name: "Alice"})
+        |CREATE (p2:Person {name: "Bob"})
+        |CREATE (p3:Person {name: "Eve"})
+        |CREATE (p4:Person {name: "Carl"})
+        |CREATE (p5:Person {name: "Richard"})
+        |CREATE (p1)-[:KNOWS]->(p2)
+        |CREATE (p2)-[:KNOWS]->(p3)
+        |CREATE (p1)-[:KNOWS]->(p3)
+        |CREATE (p3)-[:KNOWS]->(p4)
+        |CREATE (p3)-[:KNOWS]->(p5)
+      """.stripMargin)
 
-      // When
-      val result = given.cypher(
-        """
-          |MATCH (p1:Person)-[e1:KNOWS]->(p2:Person),
-          |(p2)-[e2:KNOWS]->(p3:Person),
-          |(p1)-[e3:KNOWS]->(p3),
-          |(p3)-[e4:KNOWS]->(p4)
-          |RETURN p1.name, p2.name, p3.name, p4.name
-        """.stripMargin)
+    // When
+    val result = given.cypher(
+      """
+        |MATCH (p1:Person)-[e1:KNOWS]->(p2:Person),
+        |(p2)-[e2:KNOWS]->(p3:Person),
+        |(p1)-[e3:KNOWS]->(p3),
+        |(p3)-[e4:KNOWS]->(p4)
+        |RETURN p1.name, p2.name, p3.name, p4.name
+      """.stripMargin)
 
-      // Then
-      result.getRecords.toMaps should equal(Bag(
-        CypherMap(
-          "p1.name" -> "Alice",
-          "p2.name" -> "Bob",
-          "p3.name" -> "Eve",
-          "p4.name" -> "Carl"
-        ),
-        CypherMap(
-          "p1.name" -> "Alice",
-          "p2.name" -> "Bob",
-          "p3.name" -> "Eve",
-          "p4.name" -> "Richard"
-        )
-      ))
-    }
-
-    test("test expand into for triangle") {
-      // Given
-      val given = initGraph(
-        """
-          |CREATE (p1:Person {name: "Alice"})
-          |CREATE (p2:Person {name: "Bob"})
-          |CREATE (p3:Person {name: "Eve"})
-          |CREATE (p1)-[:KNOWS]->(p2)
-          |CREATE (p2)-[:KNOWS]->(p3)
-          |CREATE (p1)-[:KNOWS]->(p3)
-        """.stripMargin)
-
-      // When
-      val result = given.cypher(
-        """
-          |MATCH (p1:Person)-[e1:KNOWS]->(p2:Person),
-          |(p2)-[e2:KNOWS]->(p3:Person),
-          |(p1)-[e3:KNOWS]->(p3)
-          |RETURN p1.name, p2.name, p3.name
-        """.stripMargin)
-
-      // Then
-      result.getRecords.toMaps should equal(Bag(
-        CypherMap(
-          "p1.name" -> "Alice",
-          "p2.name" -> "Bob",
-          "p3.name" -> "Eve"
-        )
-      ))
-    }
-
-    test("Expand into after var expand") {
-      // Given
-      val given = initGraph(
-        """
-          |CREATE (p1:Person {name: "Alice"})
-          |CREATE (p2:Person {name: "Bob"})
-          |CREATE (comment:Comment)
-          |CREATE (post1:Post {content: "asdf"})
-          |CREATE (post2:Post {content: "foobar"})
-          |CREATE (p1)-[:KNOWS]->(p2)
-          |CREATE (p2)<-[:HASCREATOR]-(comment)
-          |CREATE (comment)-[:REPLYOF]->(post1)-[:REPLYOF]->(post2)
-          |CREATE (post2)-[:HASCREATOR]->(p1)
-        """.stripMargin)
-
-      // When
-      val result = given.cypher(
-        """
-          |MATCH (p1:Person)-[e1:KNOWS]->(p2:Person),
-          |      (p2)<-[e2:HASCREATOR]-(comment:Comment),
-          |      (comment)-[e3:REPLYOF*1..10]->(post:Post),
-          |      (p1)<-[:HASCREATOR]-(post)
-          |WHERE p1.name = "Alice"
-          |RETURN p1.name, p2.name, post.content
-        """.stripMargin
+    // Then
+    result.getRecords.toMaps should equal(Bag(
+      CypherMap(
+        "p1.name" -> "Alice",
+        "p2.name" -> "Bob",
+        "p3.name" -> "Eve",
+        "p4.name" -> "Carl"
+      ),
+      CypherMap(
+        "p1.name" -> "Alice",
+        "p2.name" -> "Bob",
+        "p3.name" -> "Eve",
+        "p4.name" -> "Richard"
       )
+    ))
+  }
 
-      // Then
-      result.getRecords.toMaps should equal(Bag(
-        CypherMap(
-          "p1.name" -> "Alice",
-          "p2.name" -> "Bob",
-          "post.content" -> "foobar"
-        )
-      ))
-    }
+  test("test expand into for triangle") {
+    // Given
+    val given = initGraph(
+      """
+        |CREATE (p1:Person {name: "Alice"})
+        |CREATE (p2:Person {name: "Bob"})
+        |CREATE (p3:Person {name: "Eve"})
+        |CREATE (p1)-[:KNOWS]->(p2)
+        |CREATE (p2)-[:KNOWS]->(p3)
+        |CREATE (p1)-[:KNOWS]->(p3)
+      """.stripMargin)
+
+    // When
+    val result = given.cypher(
+      """
+        |MATCH (p1:Person)-[e1:KNOWS]->(p2:Person),
+        |(p2)-[e2:KNOWS]->(p3:Person),
+        |(p1)-[e3:KNOWS]->(p3)
+        |RETURN p1.name, p2.name, p3.name
+      """.stripMargin)
+
+    // Then
+    result.getRecords.toMaps should equal(Bag(
+      CypherMap(
+        "p1.name" -> "Alice",
+        "p2.name" -> "Bob",
+        "p3.name" -> "Eve"
+      )
+    ))
+  }
+
+  test("Expand into after var expand") {
+    // Given
+    val given = initGraph(
+      """
+        |CREATE (p1:Person {name: "Alice"})
+        |CREATE (p2:Person {name: "Bob"})
+        |CREATE (comment:Comment)
+        |CREATE (post1:Post {content: "asdf"})
+        |CREATE (post2:Post {content: "foobar"})
+        |CREATE (p1)-[:KNOWS]->(p2)
+        |CREATE (p2)<-[:HASCREATOR]-(comment)
+        |CREATE (comment)-[:REPLYOF]->(post1)-[:REPLYOF]->(post2)
+        |CREATE (post2)-[:HASCREATOR]->(p1)
+      """.stripMargin)
+
+    // When
+    val result = given.cypher(
+      """
+        |MATCH (p1:Person)-[e1:KNOWS]->(p2:Person),
+        |      (p2)<-[e2:HASCREATOR]-(comment:Comment),
+        |      (comment)-[e3:REPLYOF*1..10]->(post:Post),
+        |      (p1)<-[:HASCREATOR]-(post)
+        |WHERE p1.name = "Alice"
+        |RETURN p1.name, p2.name, post.content
+      """.stripMargin
+    )
+
+    // Then
+    result.getRecords.toMaps should equal(Bag(
+      CypherMap(
+        "p1.name" -> "Alice",
+        "p2.name" -> "Bob",
+        "post.content" -> "foobar"
+      )
+    ))
   }
 }
--- spark-cypher/src/test/scala/org/opencypher/spark/impl/acceptance/WithBehaviour.scala
+++ spark-cypher/src/test/scala/org/opencypher/spark/impl/acceptance/WithBehaviour.scala
@@ -30,326 +30,328 @@ import org.opencypher.okapi.api.value.CypherValue._
 import org.opencypher.okapi.ir.test.support.Bag
 import org.opencypher.okapi.ir.test.support.Bag._
 import org.opencypher.spark.impl.CAPSConverters._
-import org.opencypher.spark.impl.CAPSGraph
+import org.opencypher.spark.test.CAPSTestSuite
+import org.scalatest.DoNotDiscover
 
-trait WithBehaviour { this: AcceptanceTest =>
+@DoNotDiscover
+class WithBehaviour extends CAPSTestSuite with DefaultGraphInit {
 
-  def withBehaviour(initGraph: String => CAPSGraph): Unit = {
-    test("rebinding of dropped variables") {
+  test("rebinding of dropped variables") {
+    // Given
+    val given = initGraph("""CREATE (:Node {val: 1}), (:Node {val: 2})""")
+
+    // When
+    val result = given.cypher(
+      """MATCH (n:Node)
+        |WITH n.val AS foo
+        |WITH foo + 2 AS bar
+        |WITH bar + 2 AS foo
+        |RETURN foo
+      """.stripMargin)
+
+    // Then
+    result.getRecords.toMaps should equal(
+      Bag(
+        CypherMap("foo" -> 5),
+        CypherMap("foo" -> 6)
+      ))
+  }
+
+  test("projecting constants") {
+    // Given
+    val given = initGraph("""CREATE (), ()""")
+
+    // When
+    val result = given.cypher(
+      """MATCH ()
+        |WITH 3 AS foo
+        |WITH foo + 2 AS bar
+        |RETURN bar
+      """.stripMargin)
+
+    // Then
+    result.getRecords.toMaps should equal(
+      Bag(
+        CypherMap("bar" -> 5),
+        CypherMap("bar" -> 5)
+      ))
+  }
+
+  test("projecting variables in scope") {
+
+    // Given
+    val given = initGraph("""CREATE (:Node {val: 4})-[:Rel]->(:Node {val: 5})""")
+
+    // When
+    val result = given.cypher("MATCH (n:Node)-->(m:Node) WITH n, m RETURN n.val")
+
+    // Then
+    result.getRecords.toMaps should equal(
+      Bag(
+        CypherMap("n.val" -> 4)
+      ))
+  }
+
+  test("projecting property expression") {
+
+    // Given
+    val given = initGraph("""CREATE (:Node {val: 4})-[:Rel]->(:Node {val: 5})""")
+
+    // When
+    val result = given.cypher("MATCH (n:Node)-->(m:Node) WITH n.val AS n_val RETURN n_val")
+
+    // Then
+    result.getRecords.toMaps should equal(
+      Bag(
+        CypherMap("n_val" -> 4)
+      ))
+  }
+
+  test("projecting property expression with filter") {
+
+    // Given
+    val given = initGraph("""CREATE (:Node {val: 3}), (:Node {val: 4}), (:Node {val: 5})""")
+
+    // When
+    val result = given.cypher("MATCH (n:Node) WITH n.val AS n_val WHERE n_val <= 4 RETURN n_val")
+
+    // Then
+    result.getRecords.toMaps should equal(
+      Bag(
+        CypherMap("n_val" -> 3),
+        CypherMap("n_val" -> 4)
+      ))
+  }
+
+  test("projecting addition expression") {
+
+    // Given
+    val given = initGraph("""CREATE (:Node {val: 4})-[:Rel]->(:Node {val: 5})""")
+
+    // When
+    val result = given.cypher("MATCH (n:Node)-->(m:Node) WITH n.val + m.val AS sum_n_m_val RETURN sum_n_m_val")
+
+    // Then
+    result.getRecords.toMaps should equal(
+      Bag(
+        CypherMap("sum_n_m_val" -> 9)
+      ))
+  }
+
+  test("aliasing variables") {
+
+    // Given
+    val given = initGraph("""CREATE (:Node {val: 4})-[:Rel]->(:Node {val: 5})""")
+
+    // When
+    val result = given.cypher("MATCH (n:Node)-[r]->(m:Node) WITH n.val + m.val AS sum WITH sum AS sum2 RETURN sum2")
+
+    // Then
+    result.getRecords.toMaps should equal(
+      Bag(
+        CypherMap("sum2" -> 9)
+      ))
+  }
+
+  test("projecting mixed expression") {
+
+    // Given
+    val given = initGraph("""CREATE (:Node {val: 4})-[:Rel]->(:Node {val: 5})-[:Rel]->(:Node)""")
+
+    // When
+    val result = given.cypher(
+      "MATCH (n:Node)-[r]->(m:Node) WITH n.val AS n_val, n.val + m.val AS sum_n_m_val RETURN sum_n_m_val, n_val")
+
+    // Then
+    result.getRecords.toMaps should equal(
+      Bag(
+        CypherMap("sum_n_m_val" -> 9, "n_val" -> 4),
+        CypherMap("sum_n_m_val" -> null, "n_val" -> 5)
+      ))
+  }
+
+  it("can project and predicates") {
+    val graph = initGraph(
+      """
+        |CREATE ({val1: 1, val2: 3, val3: 10}), ({val1: 1, val2: 2, val3: 3})
+      """.stripMargin)
+
+    val result = graph.cypher(
+      """
+        |MATCH (n)
+        |WITH n.val1 AS val1, n.val2 AS val2, n.val3 AS val3
+        |WHERE val1 >= 1 AND val2 > 2 AND val3 > 5
+        |RETURN val1, val2, val3
+      """.stripMargin)
+
+    result.getRecords.collect.toBag should equal(Bag(
+      CypherMap("val1" -> 1, "val2" -> 3, "val3" -> 10)
+    ))
+  }
+
+  test("order by") {
+    val given = initGraph("""CREATE (:Node {val: 4}),(:Node {val: 3}),(:Node  {val: 42})""")
+
+    val result = given.cypher("MATCH (a) WITH a.val as val ORDER BY val RETURN val")
+
+    // Then
+    result.getRecords.toMaps should equal(
+      Bag(
+        CypherMap("val" -> 3L),
+        CypherMap("val" -> 4L),
+        CypherMap("val" -> 42L)
+      ))
+  }
+
+  test("order by asc") {
+    val given = initGraph("""CREATE (:Node {val: 4}),(:Node {val: 3}),(:Node  {val: 42})""")
+
+    val result = given.cypher("MATCH (a) WITH a.val as val ORDER BY val ASC RETURN val")
+
+    // Then
+    result.getRecords.toMaps should equal(
+      Bag(
+        CypherMap("val" -> 3L),
+        CypherMap("val" -> 4L),
+        CypherMap("val" -> 42L)
+      ))
+  }
+
+  test("order by desc") {
+    val given = initGraph("""CREATE (:Node {val: 4}),(:Node {val: 3}),(:Node  {val: 42})""")
+
+    val result = given.cypher("MATCH (a) WITH a.val as val ORDER BY val DESC RETURN val")
+
+    // Then
+    result.getRecords.toMaps should equal(
+      Bag(
+        CypherMap("val" -> 42L),
+        CypherMap("val" -> 4L),
+        CypherMap("val" -> 3L)
+      ))
+  }
+
+  test("skip") {
+    val given = initGraph("""CREATE (:Node {val: 4}),(:Node {val: 3}),(:Node  {val: 42})""")
+
+    val result = given.cypher("MATCH (a) WITH a.val as val SKIP 2 RETURN val").asCaps
+
+    // Then
+    result.getRecords.toDF().count() should equal(1)
+  }
+
+  test("order by with skip") {
+    val given = initGraph("""CREATE (:Node {val: 4}),(:Node {val: 3}),(:Node  {val: 42})""")
+
+    val result = given.cypher("MATCH (a) WITH a.val as val ORDER BY val SKIP 1 RETURN val")
+
+    // Then
+    result.getRecords.toMaps should equal(
+      Bag(
+        CypherMap("val" -> 4L),
+        CypherMap("val" -> 42L)
+      ))
+  }
+
+  test("order by with (arithmetic) skip") {
+    val given = initGraph("""CREATE (:Node {val: 4}),(:Node {val: 3}),(:Node  {val: 42})""")
+
+    val result = given.cypher("MATCH (a) WITH a.val as val ORDER BY val SKIP 1 + 1 RETURN val")
+
+    // Then
+    result.getRecords.toMaps should equal(
+      Bag(
+        CypherMap("val" -> 42L)
+      ))
+  }
+
+  test("limit") {
+    val given = initGraph("""CREATE (:Node {val: 4}),(:Node {val: 3}),(:Node  {val: 42})""")
+
+    val result = given.cypher("MATCH (a) WITH a.val as val LIMIT 1 RETURN val").asCaps
+
+    // Then
+    result.getRecords.toDF().count() should equal(1)
+  }
+
+  test("order by with limit") {
+    val given = initGraph("""CREATE (:Node {val: 4}),(:Node {val: 3}),(:Node  {val: 42})""")
+
+    val result = given.cypher("MATCH (a) WITH a.val as val ORDER BY val LIMIT 1 RETURN val")
+
+    // Then
+    result.getRecords.toMaps should equal(
+      Bag(
+        CypherMap("val" -> 3L)
+      ))
+  }
+
+  test("order by with (arithmetic) limit") {
+    val given = initGraph("""CREATE (:Node {val: 4}),(:Node {val: 3}),(:Node  {val: 42})""")
+
+    val result = given.cypher("MATCH (a) WITH a.val as val ORDER BY val LIMIT 1 + 1 RETURN val")
+
+    // Then
+    result.getRecords.toMaps should equal(
+      Bag(
+        CypherMap("val" -> 3L),
+        CypherMap("val" -> 4L)
+      ))
+  }
+
+  test("order by with skip and limit") {
+    val given = initGraph("""CREATE (:Node {val: 4}),(:Node {val: 3}),(:Node  {val: 42})""")
+
+    val result = given.cypher("MATCH (a) WITH a.val as val ORDER BY val SKIP 1 LIMIT 1 RETURN val")
+
+    // Then
+    result.getRecords.toMaps should equal(
+      Bag(
+        CypherMap("val" -> 4L)
+      ))
+  }
+
+
+  describe("NOT") {
+    it("can project not of literal") {
       // Given
-      val given = initGraph("""CREATE (:Node {val: 1}), (:Node {val: 2})""")
-
-      // When
-      val result = given.cypher("""MATCH (n:Node)
-          |WITH n.val AS foo
-          |WITH foo + 2 AS bar
-          |WITH bar + 2 AS foo
-          |RETURN foo
+      val given = initGraph(
+        """
+          |CREATE ()
         """.stripMargin)
-
-      // Then
-      result.getRecords.toMaps should equal(
-        Bag(
-          CypherMap("foo" -> 5),
-          CypherMap("foo" -> 6)
-        ))
-    }
-
-    test("projecting constants") {
-      // Given
-      val given = initGraph("""CREATE (), ()""")
-
-      // When
-      val result = given.cypher("""MATCH ()
-          |WITH 3 AS foo
-          |WITH foo + 2 AS bar
-          |RETURN bar
-        """.stripMargin)
-
-      // Then
-      result.getRecords.toMaps should equal(
-        Bag(
-          CypherMap("bar" -> 5),
-          CypherMap("bar" -> 5)
-        ))
-    }
-
-    test("projecting variables in scope") {
-
-      // Given
-      val given = initGraph("""CREATE (:Node {val: 4})-[:Rel]->(:Node {val: 5})""")
-
-      // When
-      val result = given.cypher("MATCH (n:Node)-->(m:Node) WITH n, m RETURN n.val")
-
-      // Then
-      result.getRecords.toMaps should equal(
-        Bag(
-          CypherMap("n.val" -> 4)
-        ))
-    }
-
-    test("projecting property expression") {
-
-      // Given
-      val given = initGraph("""CREATE (:Node {val: 4})-[:Rel]->(:Node {val: 5})""")
-
-      // When
-      val result = given.cypher("MATCH (n:Node)-->(m:Node) WITH n.val AS n_val RETURN n_val")
-
-      // Then
-      result.getRecords.toMaps should equal(
-        Bag(
-          CypherMap("n_val" -> 4)
-        ))
-    }
-
-    test("projecting property expression with filter") {
-
-      // Given
-      val given = initGraph("""CREATE (:Node {val: 3}), (:Node {val: 4}), (:Node {val: 5})""")
-
-      // When
-      val result = given.cypher("MATCH (n:Node) WITH n.val AS n_val WHERE n_val <= 4 RETURN n_val")
-
-      // Then
-      result.getRecords.toMaps should equal(
-        Bag(
-          CypherMap("n_val" -> 3),
-          CypherMap("n_val" -> 4)
-        ))
-    }
-
-    test("projecting addition expression") {
-
-      // Given
-      val given = initGraph("""CREATE (:Node {val: 4})-[:Rel]->(:Node {val: 5})""")
-
-      // When
-      val result = given.cypher("MATCH (n:Node)-->(m:Node) WITH n.val + m.val AS sum_n_m_val RETURN sum_n_m_val")
-
-      // Then
-      result.getRecords.toMaps should equal(
-        Bag(
-          CypherMap("sum_n_m_val" -> 9)
-        ))
-    }
-
-    test("aliasing variables") {
-
-      // Given
-      val given = initGraph("""CREATE (:Node {val: 4})-[:Rel]->(:Node {val: 5})""")
-
-      // When
-      val result = given.cypher("MATCH (n:Node)-[r]->(m:Node) WITH n.val + m.val AS sum WITH sum AS sum2 RETURN sum2")
-
-      // Then
-      result.getRecords.toMaps should equal(
-        Bag(
-          CypherMap("sum2" -> 9)
-        ))
-    }
-
-    test("projecting mixed expression") {
-
-      // Given
-      val given = initGraph("""CREATE (:Node {val: 4})-[:Rel]->(:Node {val: 5})-[:Rel]->(:Node)""")
 
       // When
       val result = given.cypher(
-        "MATCH (n:Node)-[r]->(m:Node) WITH n.val AS n_val, n.val + m.val AS sum_n_m_val RETURN sum_n_m_val, n_val")
+        """
+          |WITH true AS t, false AS f
+          |WITH NOT true AS nt, not false AS nf
+          |RETURN nt, nf""".stripMargin)
 
       // Then
-      result.getRecords.toMaps should equal(
-        Bag(
-          CypherMap("sum_n_m_val" -> 9, "n_val" -> 4),
-          CypherMap("sum_n_m_val" -> null, "n_val" -> 5)
-        ))
-    }
-
-    it("can project and predicates") {
-      val graph = initGraph(
-        """
-          |CREATE ({val1: 1, val2: 3, val3: 10}), ({val1: 1, val2: 2, val3: 3})
-        """.stripMargin)
-
-      val result = graph.cypher(
-        """
-          |MATCH (n)
-          |WITH n.val1 AS val1, n.val2 AS val2, n.val3 AS val3
-          |WHERE val1 >= 1 AND val2 > 2 AND val3 > 5
-          |RETURN val1, val2, val3
-        """.stripMargin)
-
-      result.getRecords.collect.toBag should equal(Bag(
-        CypherMap("val1" -> 1, "val2" -> 3, "val3" -> 10)
+      result.getRecords.toMaps should equal(Bag(
+        CypherMap("nt" -> false, "nf" -> true)
       ))
     }
 
-    test("order by") {
-      val given = initGraph("""CREATE (:Node {val: 4}),(:Node {val: 3}),(:Node  {val: 42})""")
+    it("can project not of expression") {
+      // Given
+      val given = initGraph(
+        """
+          |CREATE ({id: 1, val: true}), ({id: 2, val: false})
+        """.stripMargin)
 
-      val result = given.cypher("MATCH (a) WITH a.val as val ORDER BY val RETURN val")
-
-      // Then
-      result.getRecords.toMaps should equal(
-        Bag(
-          CypherMap("val" -> 3L),
-          CypherMap("val" -> 4L),
-          CypherMap("val" -> 42L)
-        ))
-    }
-
-    test("order by asc") {
-      val given = initGraph("""CREATE (:Node {val: 4}),(:Node {val: 3}),(:Node  {val: 42})""")
-
-      val result = given.cypher("MATCH (a) WITH a.val as val ORDER BY val ASC RETURN val")
+      // When
+      val result = given.cypher(
+        """
+          |MATCH (n)
+          |WITH n.id AS id, NOT n.val AS val2
+          |RETURN id, val2""".stripMargin)
 
       // Then
-      result.getRecords.toMaps should equal(
-        Bag(
-          CypherMap("val" -> 3L),
-          CypherMap("val" -> 4L),
-          CypherMap("val" -> 42L)
-        ))
-    }
-
-    test("order by desc") {
-      val given = initGraph("""CREATE (:Node {val: 4}),(:Node {val: 3}),(:Node  {val: 42})""")
-
-      val result = given.cypher("MATCH (a) WITH a.val as val ORDER BY val DESC RETURN val")
-
-      // Then
-      result.getRecords.toMaps should equal(
-        Bag(
-          CypherMap("val" -> 42L),
-          CypherMap("val" -> 4L),
-          CypherMap("val" -> 3L)
-        ))
-    }
-
-    test("skip") {
-      val given = initGraph("""CREATE (:Node {val: 4}),(:Node {val: 3}),(:Node  {val: 42})""")
-
-      val result = given.cypher("MATCH (a) WITH a.val as val SKIP 2 RETURN val").asCaps
-
-      // Then
-      result.getRecords.toDF().count() should equal(1)
-    }
-
-    test("order by with skip") {
-      val given = initGraph("""CREATE (:Node {val: 4}),(:Node {val: 3}),(:Node  {val: 42})""")
-
-      val result = given.cypher("MATCH (a) WITH a.val as val ORDER BY val SKIP 1 RETURN val")
-
-      // Then
-      result.getRecords.toMaps should equal(
-        Bag(
-          CypherMap("val" -> 4L),
-          CypherMap("val" -> 42L)
-        ))
-    }
-
-    test("order by with (arithmetic) skip") {
-      val given = initGraph("""CREATE (:Node {val: 4}),(:Node {val: 3}),(:Node  {val: 42})""")
-
-      val result = given.cypher("MATCH (a) WITH a.val as val ORDER BY val SKIP 1 + 1 RETURN val")
-
-      // Then
-      result.getRecords.toMaps should equal(
-        Bag(
-          CypherMap("val" -> 42L)
-        ))
-    }
-
-    test("limit") {
-      val given = initGraph("""CREATE (:Node {val: 4}),(:Node {val: 3}),(:Node  {val: 42})""")
-
-      val result = given.cypher("MATCH (a) WITH a.val as val LIMIT 1 RETURN val").asCaps
-
-      // Then
-      result.getRecords.toDF().count() should equal(1)
-    }
-
-    test("order by with limit") {
-      val given = initGraph("""CREATE (:Node {val: 4}),(:Node {val: 3}),(:Node  {val: 42})""")
-
-      val result = given.cypher("MATCH (a) WITH a.val as val ORDER BY val LIMIT 1 RETURN val")
-
-      // Then
-      result.getRecords.toMaps should equal(
-        Bag(
-          CypherMap("val" -> 3L)
-        ))
-    }
-
-    test("order by with (arithmetic) limit") {
-      val given = initGraph("""CREATE (:Node {val: 4}),(:Node {val: 3}),(:Node  {val: 42})""")
-
-      val result = given.cypher("MATCH (a) WITH a.val as val ORDER BY val LIMIT 1 + 1 RETURN val")
-
-      // Then
-      result.getRecords.toMaps should equal(
-        Bag(
-          CypherMap("val" -> 3L),
-          CypherMap("val" -> 4L)
-        ))
-    }
-
-    test("order by with skip and limit") {
-      val given = initGraph("""CREATE (:Node {val: 4}),(:Node {val: 3}),(:Node  {val: 42})""")
-
-      val result = given.cypher("MATCH (a) WITH a.val as val ORDER BY val SKIP 1 LIMIT 1 RETURN val")
-
-      // Then
-      result.getRecords.toMaps should equal(
-        Bag(
-          CypherMap("val" -> 4L)
-        ))
-    }
-
-
-    describe("NOT") {
-      it("can project not of literal") {
-        // Given
-        val given = initGraph(
-          """
-            |CREATE ()
-          """.stripMargin)
-
-        // When
-        val result = given.cypher(
-          """
-            |WITH true AS t, false AS f
-            |WITH NOT true AS nt, not false AS nf
-            |RETURN nt, nf""".stripMargin)
-
-        // Then
-        result.getRecords.toMaps should equal(Bag(
-          CypherMap("nt" -> false, "nf" -> true)
-        ))
-      }
-
-      it("can project not of expression") {
-        // Given
-        val given = initGraph(
-          """
-            |CREATE ({id: 1, val: true}), ({id: 2, val: false})
-          """.stripMargin)
-
-        // When
-        val result = given.cypher(
-          """
-            |MATCH (n)
-            |WITH n.id AS id, NOT n.val AS val2
-            |RETURN id, val2""".stripMargin)
-
-        // Then
-        result.getRecords.toMaps should equal(Bag(
-          CypherMap("id" -> 1L, "val2" -> false),
-          CypherMap("id" -> 2L, "val2" -> true)
-        ))
-      }
+      result.getRecords.toMaps should equal(Bag(
+        CypherMap("id" -> 1L, "val2" -> false),
+        CypherMap("id" -> 2L, "val2" -> true)
+      ))
     }
   }
 }
