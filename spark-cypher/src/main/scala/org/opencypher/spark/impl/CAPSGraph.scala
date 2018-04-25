6e758ae0696648031b70bc139d29f09d95bf8695
Martin Junghanns <martin.junghanns@gmx.net>
2018-04-25 20:30:17 +0200

    Fix property slot selection in nodesByExactLabel
    #395

--- spark-cypher/src/main/scala/org/opencypher/spark/impl/CAPSGraph.scala
+++ spark-cypher/src/main/scala/org/opencypher/spark/impl/CAPSGraph.scala
@@ -27,17 +27,17 @@
 package org.opencypher.spark.impl
 
 import org.apache.spark.storage.StorageLevel
-import org.opencypher.okapi.api.graph.{GraphOperations, PropertyGraph, QualifiedGraphName}
+import org.opencypher.okapi.api.graph.{GraphOperations, PropertyGraph}
 import org.opencypher.okapi.api.schema._
 import org.opencypher.okapi.api.table.CypherRecords
 import org.opencypher.okapi.api.types.{CTNode, CTRelationship}
 import org.opencypher.okapi.impl.exception.IllegalArgumentException
+import org.opencypher.okapi.ir.api.PropertyKey
 import org.opencypher.okapi.ir.api.expr._
 import org.opencypher.okapi.relational.impl.table.{ColumnName, OpaqueField, RecordHeader}
 import org.opencypher.spark.api.CAPSSession
 import org.opencypher.spark.api.io.{CAPSEntityTable, CAPSNodeTable}
 import org.opencypher.spark.impl.CAPSConverters._
-import org.opencypher.spark.impl.util.TagSupport.computeRetaggings
 import org.opencypher.spark.schema.CAPSSchema
 import org.opencypher.spark.schema.CAPSSchema._
 
@@ -72,21 +72,39 @@ trait CAPSGraph extends PropertyGraph with GraphOperations with Serializable {
     val nodeVar = Var(name)(nodeType)
     val records = nodes(name, nodeType)
 
-    // compute slot contents to remove and to keep
-    val removeSlots = records.header.labelSlots(nodeVar)
-      .filterNot(slot => labels.contains(slot._1.label.name))
+    // compute slot contents to keep
+    val idSlot = records.header.slotFor(nodeVar)
+
+    val labelSlots = records.header.labelSlots(nodeVar)
+      .filter(slot => labels.contains(slot._1.label.name))
       .values
-    val keepSlots = records.header.contents.diff(removeSlots.map(_.content).toSet)
+
+    val propertySlots = schema.nodeKeys(labels).flatMap {
+      case (propertyName, cypherType) =>
+        records.header.slotsFor(Property(nodeVar, PropertyKey(propertyName))(cypherType))
+    }
+
+    val keepSlots = (Seq(idSlot) ++ labelSlots ++ propertySlots).map(_.content).toSet
+    val keepCols = keepSlots.map(ColumnName.of).toSeq
 
     // we only keep rows where all "other" labels are false
-    val predicate = removeSlots.map(ColumnName.of)
+    val predicate = records.header.labelSlots(nodeVar)
+      .filterNot(slot => labels.contains(slot._1.label.name))
+      .values
+      .map(ColumnName.of)
       .map(records.data.col(_) === false)
       .reduceOption(_ && _)
 
-    // filter rows and drop unnecessary columns
+    // filter rows and select only necessary columns
     val updatedData = predicate match {
-      case Some(filter) => records.data.filter(filter).drop(removeSlots.map(ColumnName.of).toSeq :_*)
-      case None => records.data
+
+      case Some(filter) =>
+        records.data
+          .filter(filter)
+          .select(keepCols.head, keepCols.tail: _*)
+
+      case None =>
+        records.data.select(keepCols.head, keepCols.tail: _*)
     }
 
     val updatedHeader = RecordHeader.from(keepSlots.toSeq: _*)
@@ -128,7 +146,7 @@ object CAPSGraph {
   }
 
   def createLazy(theSchema: CAPSSchema, loadGraph: => CAPSGraph)(implicit caps: CAPSSession): CAPSGraph =
-    new LazyGraph(theSchema, loadGraph) { }
+    new LazyGraph(theSchema, loadGraph) {}
 
   sealed abstract class LazyGraph(override val schema: CAPSSchema, loadGraph: => CAPSGraph)(implicit caps: CAPSSession)
     extends CAPSGraph {
