39cbcd7666c4ed3c35e19c7046057c54a5b8ddcf
Martin Junghanns <m.junghanns@mailbox.org>
2018-05-31 16:42:14 +0200

    Change to flattened alias representation
    Co-Authored-By: Philip Stutz <philip.stutz@gmail.com>

--- okapi-relational/src/test/scala/org/opencypher/okapi/relational/impl/table/RecordHeaderNewTest.scala
+++ okapi-relational/src/test/scala/org/opencypher/okapi/relational/impl/table/RecordHeaderNewTest.scala
@@ -26,63 +26,78 @@
  */
 package org.opencypher.okapi.relational.impl.table
 
-import org.opencypher.okapi.api.types.{CTBoolean, CTNode, CTString}
+import org.opencypher.okapi.api.types.{CTBoolean, CTInteger, CTNode, CTString}
+import org.opencypher.okapi.ir.api.expr._
 import org.opencypher.okapi.ir.api.{Label, PropertyKey}
-import org.opencypher.okapi.ir.api.expr.{HasLabel, Property, Var}
-import org.scalatest.{FunSpec, Matchers}
-
 import org.opencypher.okapi.ir.test.support.MatchHelper._
+import org.scalatest.{FunSpec, Matchers}
 
 class RecordHeaderNewTest extends FunSpec with Matchers {
 
-  val n = Var("n")(CTNode)
-  val m = Var("m")(CTNode)
-  val o = Var("o")(CTNode)
+  val n: Var = Var("n")(CTNode)
+  val m: Var = Var("m")(CTNode)
+  val o: Var = Var("o")(CTNode)
 
-  val label1 = HasLabel(n, Label("A"))(CTBoolean)
-  val label2 = HasLabel(n, Label("B"))(CTBoolean)
-  val prop = Property(n, PropertyKey("foo"))(CTString)
-  val exprs = Set(n, label1, label2, prop)
+  val countN = CountStar(CTInteger)
 
-  it("can add a node") {
-    val header = RecordHeaderNew.empty
-      .withExpr(n)
-      .withExpr(label1)
-      .withExpr(label2)
-      .withExpr(prop)
+  val nLabelA: HasLabel = HasLabel(n, Label("A"))(CTBoolean)
+  val nLabelB: HasLabel = HasLabel(n, Label("B"))(CTBoolean)
+  val nPropFoo: Property = Property(n, PropertyKey("foo"))(CTString)
+  val nExprs: Set[Expr] = Set(n, nLabelA, nLabelB, nPropFoo)
+  val mExprs: Set[Expr] = nExprs.map(_.withOwner(m))
+  val oExprs: Set[Expr] = nExprs.map(_.withOwner(o))
 
-    header.ownedBy(n) should equal(exprs)
+  val nHeader: RecordHeaderNew = RecordHeaderNew.empty.withExprs(nExprs)
+  val mHeader: RecordHeaderNew = RecordHeaderNew.empty.withExprs(mExprs)
+
+  it("can add an entity expression") {
+    nHeader.ownedBy(n) should equal(nExprs)
   }
 
-  it("can add a node alias") {
-    val header = RecordHeaderNew.empty
-      .withExpr(n)
-      .withExpr(label1)
-      .withExpr(label2)
-      .withExpr(prop)
+//  it("can add a non-entity expression") {
+//    val header = RecordHeaderNew.empty.withExpr(countN)
+//
+//    header.column(countN)
+//  }
 
-    val m = Var("m")(CTNode)
-    val withAlias = header.withAlias(m, n)
-    val exprsWithNewOwner = exprs.map(_.withOwner(m))
+  it("can add an alias for an entity") {
+    val withAlias = nHeader.withAlias(m, n)
 
-    withAlias.ownedBy(n) should equalWithTracing(exprs)
-    withAlias.ownedBy(m) should equalWithTracing(exprsWithNewOwner)
+    withAlias.ownedBy(n) should equalWithTracing(nExprs)
+    withAlias.ownedBy(m) should equalWithTracing(mExprs)
   }
 
-  it("add alias for non-entity expr") {
-    val header = RecordHeaderNew.empty
-      .withExpr(n)
-      .withExpr(label1)
-      .withExpr(label2)
-      .withExpr(prop)
-
-    val withAlias1 = header.withAlias(m, prop)
+  it("can add an alias for a non-entity expression") {
+    val withAlias1 = nHeader.withAlias(m, nPropFoo)
     val withAlias2 = withAlias1.withAlias(o, m)
 
 
-    withAlias2.column(o) should equalWithTracing(withAlias2.column(prop))
-    withAlias2.column(m) should equalWithTracing(withAlias2.column(prop))
-    withAlias2.ownedBy(n) should equalWithTracing(exprs)
+    withAlias2.column(o) should equalWithTracing(withAlias2.column(nPropFoo))
+    withAlias2.column(m) should equalWithTracing(withAlias2.column(nPropFoo))
+    withAlias2.ownedBy(n) should equalWithTracing(nExprs)
+    withAlias2.ownedBy(o) should equalWithTracing(Set.empty)
+    withAlias2.ownedBy(m) should equalWithTracing(Set.empty)
+  }
+
+  it("can combine simple headers") {
+    val unionHeader = nHeader ++ mHeader
+
+    unionHeader.ownedBy(n) should equalWithTracing(nExprs)
+    unionHeader.ownedBy(m) should equalWithTracing(mExprs)
+  }
+
+  it("can combine complex headers") {
+    val p = Var("nPropFoo_Alias")(nPropFoo.cypherType)
+
+    val nHeaderWithAlias = nHeader.withAlias(p, nPropFoo)
+    val mHeaderWithAlias = mHeader.withAlias(o, m)
+
+    val unionHeader = nHeaderWithAlias ++ mHeaderWithAlias
+
+    unionHeader.column(p) should equal(unionHeader.column(nPropFoo))
+    unionHeader.ownedBy(n) should equalWithTracing(nExprs)
+    unionHeader.ownedBy(m) should equalWithTracing(mExprs)
+    unionHeader.ownedBy(o) should equalWithTracing(oExprs)
   }
 
 }
