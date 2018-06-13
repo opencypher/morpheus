8168e609d007ee146d5978a47c3f8661c2704a21
Martin Junghanns <m.junghanns@mailbox.org>
2018-06-13 13:27:09 +0200

    Revert "Remove physical operator caching"
    This reverts commit 93a5faa

--- spark-cypher/src/main/scala/org/opencypher/spark/impl/physical/PhysicalOptimizer.scala
+++ spark-cypher/src/main/scala/org/opencypher/spark/impl/physical/PhysicalOptimizer.scala
@@ -27,14 +27,62 @@
 package org.opencypher.spark.impl.physical
 
 import org.opencypher.okapi.ir.api.util.DirectCompilationStage
-import org.opencypher.spark.impl.physical.operators.CAPSPhysicalOperator
+import org.opencypher.okapi.trees.TopDown
+import org.opencypher.spark.impl.physical.operators.{CAPSPhysicalOperator, Cache, Start}
 
 case class PhysicalOptimizerContext()
 
 class PhysicalOptimizer extends DirectCompilationStage[CAPSPhysicalOperator, CAPSPhysicalOperator, PhysicalOptimizerContext] {
 
   override def process(input: CAPSPhysicalOperator)(implicit context: PhysicalOptimizerContext): CAPSPhysicalOperator = {
-    input
+    InsertCachingOperators(input)
   }
 
+  object InsertCachingOperators extends (CAPSPhysicalOperator => CAPSPhysicalOperator) {
+    def apply(input: CAPSPhysicalOperator): CAPSPhysicalOperator = {
+      val replacements = calculateReplacementMap(input).filterKeys {
+        case _: Start => false
+        case _                           => true
+      }
+
+      val nodesToReplace = replacements.keySet
+
+      TopDown[CAPSPhysicalOperator] {
+        case cache: Cache => cache
+        case parent if (parent.childrenAsSet intersect nodesToReplace).nonEmpty =>
+          val newChildren = parent.children.map(c => replacements.getOrElse(c, c))
+          parent.withNewChildren(newChildren)
+      }.rewrite(input)
+    }
+
+    private def calculateReplacementMap(input: CAPSPhysicalOperator): Map[CAPSPhysicalOperator, CAPSPhysicalOperator] = {
+      val opCounts = identifyDuplicates(input)
+      val opsByHeight = opCounts.keys.toSeq.sortWith((a, b) => a.height > b.height)
+      val (opsToCache, _) = opsByHeight.foldLeft(Set.empty[CAPSPhysicalOperator] -> opCounts) { (agg, currentOp) =>
+        agg match {
+          case (currentOpsToCache: Set[CAPSPhysicalOperator], currentCounts: Map[CAPSPhysicalOperator, Int]) =>
+            val currentOpCount = currentCounts(currentOp)
+            if (currentOpCount > 1) {
+              val updatedOps = currentOpsToCache + currentOp
+              val updatedCounts = currentCounts.map {
+                case (op, count) => op -> (if (currentOp.containsTree(op)) count - 1 else count)
+              }
+              updatedOps -> updatedCounts
+            } else {
+              currentOpsToCache -> currentCounts
+            }
+        }
+      }
+
+      opsToCache.map(op => op -> Cache(op)).toMap
+    }
+
+    private def identifyDuplicates(input: CAPSPhysicalOperator): Map[CAPSPhysicalOperator, Int] = {
+      input
+        .foldLeft(Map.empty[CAPSPhysicalOperator, Int].withDefaultValue(0)) {
+          case (agg, op) => agg.updated(op, agg(op) + 1)
+        }
+        .filter(_._2 > 1)
+    }
+  }
 }
