41e5283351148bf75d706a541599aab7503ce029
Martin Junghanns <m.junghanns@mailbox.org>
2018-07-05 13:34:40 +0200

    Skip tag tests for Neo4j PGDS
    Co-Authored-By: Philip Stutz <philip.stutz@gmail.com>

--- spark-cypher-testing/src/test/scala/org/opencypher/spark/api/io/neo4j/Neo4jPGDSAcceptanceTest.scala
+++ spark-cypher-testing/src/test/scala/org/opencypher/spark/api/io/neo4j/Neo4jPGDSAcceptanceTest.scala
@@ -28,14 +28,14 @@ package org.opencypher.spark.api.io.neo4j
 
 import org.opencypher.okapi.api.graph.GraphName
 import org.opencypher.okapi.api.io.PropertyGraphDataSource
+import org.opencypher.okapi.testing.PGDSAcceptance
 import org.opencypher.okapi.testing.propertygraph.InMemoryTestGraph
 import org.opencypher.spark.api.{CAPSSession, CypherGraphSources}
 import org.opencypher.spark.testing.CAPSTestSuite
-import org.opencypher.spark.testing.api.io.CAPSPGDSAcceptance
 import org.opencypher.spark.testing.fixture.Neo4jServerFixture
 import org.opencypher.spark.testing.support.creation.caps.CAPSScanGraphFactory
 
-class Neo4jPGDSAcceptanceTest extends CAPSTestSuite with Neo4jServerFixture with CAPSPGDSAcceptance {
+class Neo4jPGDSAcceptanceTest extends CAPSTestSuite with Neo4jServerFixture with PGDSAcceptance[CAPSSession] {
 
   override def initSession(): CAPSSession = caps
 
