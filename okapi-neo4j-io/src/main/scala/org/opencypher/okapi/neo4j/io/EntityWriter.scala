d03cc1a1bf0c80c9cd7e6e689cd02705fc834648
DarthMax <max.kiessling@neo4j.com>
2018-08-02 18:04:04 +0200

    Implement multiple set based node and rel writing
    Co-Authored-By: Philip Stutz <philip.stutz@gmail.com>
    Co-Authored-By: Martin Junghanns <martin.junghanns@neotechnology.com>

--- okapi-neo4j-io/src/main/scala/org/opencypher/okapi/neo4j/io/EntityWriter.scala
+++ okapi-neo4j-io/src/main/scala/org/opencypher/okapi/neo4j/io/EntityWriter.scala
@@ -2,75 +2,22 @@ package org.opencypher.okapi.neo4j.io
 
 import org.apache.logging.log4j.scala.Logging
 import org.neo4j.driver.internal.value.{ListValue, MapValue}
-import org.neo4j.driver.v1.{Statement, Value, Values}
+import org.neo4j.driver.v1.{Statement, Value}
+import org.opencypher.okapi.neo4j.io.Neo4jHelpers.Neo4jDefaults._
 import org.opencypher.okapi.neo4j.io.Neo4jHelpers._
 
 object EntityWriter extends Logging {
 
-  def writeNodes(
-    nodes: Iterator[EntityRow],
-    propertyMapping: Array[String],
+  def writeNodes[T](
+    nodes: Iterator[T],
+    rowMapping: Array[String],
     config: Neo4jConfig,
     labels: Set[String],
     batchSize: Int = 1000
-  ): Unit = {
+  )(rowToListValue: T => ListValue): Unit = {
     val labelString = labels.mkString(":")
 
-    val reuseMap = new java.util.HashMap[String, Value]
-    val reuseParameters = new MapValue(reuseMap)
-
-    val createQ =
-      s"""
-         |UNWIND {batch} as row
-         |CREATE (n:$labelString)
-         |SET n += row
-         """.stripMargin
-
-    val reuseStatement = new Statement(createQ, reuseParameters)
-
-    config.withSession { session =>
-      val batches = nodes.grouped(batchSize)
-      while (batches.hasNext) {
-        val batch = batches.next()
-        val list = new Array[MapValue](batch.size)
-
-        var i = 0
-        batch.foreach { row =>
-          val rowParameters = new java.util.HashMap[String, Value]()
-          var j = 0
-          while (j < propertyMapping.length) {
-            val key = propertyMapping(j)
-            if (key != null) {
-              rowParameters.put(key, Values.value(row.getValue(j)))
-            }
-            j += 1
-          }
-          list(i) = new MapValue(rowParameters)
-          i += 1
-        }
-
-        reuseMap.put("batch", new ListValue(list: _*))
-
-        reuseStatement.withUpdatedParameters(reuseParameters)
-
-        session.run(reuseStatement).consume()
-      }
-    }
-  }
-
-  def writeNodesList(
-    nodes: Iterator[EntityRow],
-    propertyMapping: Array[String],
-    config: Neo4jConfig,
-    labels: Set[String],
-    batchSize: Int = 1000
-  ): Unit = {
-    val labelString = labels.mkString(":")
-
-    val reuseMap = new java.util.HashMap[String, Value]
-    val reuseParameters = new MapValue(reuseMap)
-
-    val setStatements = propertyMapping
+    val setStatements = rowMapping
       .zipWithIndex
       .filterNot(_._1 == null)
       .map{ case (key, i) => s"SET n.$key = row[$i]" }
@@ -83,21 +30,55 @@ object EntityWriter extends Logging {
          |$setStatements
          """.stripMargin
 
-    val reuseStatement = new Statement(createQ, reuseParameters)
+    writeEntities(nodes, rowMapping, createQ, config, batchSize)(rowToListValue)
+  }
+
+  def writeRelationships[T](
+    relationships: Iterator[T],
+    startNodeIndex: Int,
+    endNodeIndex: Int,
+    rowMapping: Array[String],
+    config: Neo4jConfig,
+    relType: String,
+    batchSize: Int = 1000
+  )(rowToListValue: T => ListValue): Unit = {
+    val setStatements = rowMapping
+      .zipWithIndex
+      .filterNot(_._1 == null)
+      .map{ case (key, i) => s"SET n.$key = row[$i]" }
+      .mkString("\n")
+
+    val createQ =
+      s"""
+         |MATCH (from {$metaPropertyKey : row[$startNodeIndex]})
+         |MATCH (to {$metaPropertyKey : row[$endNodeIndex]})
+         |CREATE (from)-[rel:$relType]->(to)
+         |$setStatements
+         """.stripMargin
+
+    writeEntities(relationships, rowMapping, createQ, config, batchSize)(rowToListValue)
+  }
+
+  private def writeEntities[T](
+    entities: Iterator[T],
+    rowMapping: Array[String],
+    query: String,
+    config: Neo4jConfig,
+    batchSize: Int = 1000
+  )(rowToListValue: T => ListValue): Unit = {
+    val reuseMap = new java.util.HashMap[String, Value]
+    val reuseParameters = new MapValue(reuseMap)
+    val reuseStatement = new Statement(query, reuseParameters)
 
     config.withSession { session =>
-      val batches = nodes.grouped(batchSize)
+      val batches = entities.grouped(batchSize)
       while (batches.hasNext) {
         val batch = batches.next()
-        val list = new Array[ListValue](batch.size)
+        val rowParameters = new Array[ListValue](batch.size)
 
-        var i = 0
-        batch.foreach { row =>
-          list(i) = row.asListValue
-          i += 1
-        }
+        batch.zipWithIndex.foreach { case (row, i) => rowParameters(i) = rowToListValue(row) }
 
-        reuseMap.put("batch", new ListValue(list: _*))
+        reuseMap.put("batch", new ListValue(rowParameters: _*))
 
         reuseStatement.withUpdatedParameters(reuseParameters)
 
@@ -105,45 +86,4 @@ object EntityWriter extends Logging {
       }
     }
   }
-
-  def writeRelationships(
-    relationships: Iterator[EntityRow],
-    startNodeIndex: Int,
-    endNodeIndex: Int,
-    propertyMapping: Map[String, Int],
-    config: Neo4jConfig,
-    relType: String,
-    batchSize: Int = 1000
-  ): Unit = {
-
-    //    config.withSession { session =>
-    //      relationships.grouped(batchSize).foreach { batch =>
-    //
-    //        val list = new Array[RelationshipMap](batch.size)
-    //        batch.indices.foreach { i =>
-    //          list(i) = new RelationshipMap(startNodeIndex, endNodeIndex, new PropertyMap(batch(i), propertyMapping))
-    //        }
-    //
-    //        val params = new java.util.HashMap[String, AnyRef]()
-    //        params.put("batch", list)
-    //
-    //        val createQ =
-    //          s"""
-    //             |UNWIND {batch} as row
-    //             |MATCH (from {$metaPropertyKey : row.$START_NODE_KEY})
-    //             |MATCH (to {$metaPropertyKey : row.$END_NODE_KEY})
-    //             |CREATE (from)-[rel:$relType]->(to)
-    //             |SET rel += row.$PROPERTIES_KEY
-    //         """.stripMargin
-    //
-    //        session.run(createQ, params).consume()
-    //      }
-    //    }
-  }
-}
-
-trait EntityRow {
-  def getValue(i: Int): AnyRef
-  def size: Int
-  def asListValue: ListValue
 }
