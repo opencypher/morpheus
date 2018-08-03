0a0dc171fa6d45ce924686b09b663338863bccf1
pstutz <philip.stutz@gmail.com>
2018-08-03 12:11:34 +0100

    Add tree transformer and general concept of transformers

--- okapi-trees/src/main/scala/org/opencypher/okapi/trees/TreeTransformer.scala
+++ okapi-trees/src/main/scala/org/opencypher/okapi/trees/TreeTransformer.scala
@@ -28,14 +28,17 @@ package org.opencypher.okapi.trees
 
 import scala.reflect.ClassTag
 
-abstract class TreeRewriter[I <: TreeNode[I]: ClassTag, O <: TreeNode[O]: ClassTag] {
+
+abstract class TreeTransformer[I <: TreeNode[I] : ClassTag, O] {
   def rewrite(tree: I): O
 }
+
+abstract class TreeRewriter[T <: TreeNode[T] : ClassTag] extends TreeTransformer[T, T]
 
 /**
   * Applies the given partial function starting from the leafs of this tree.
   */
-case class BottomUp[T <: TreeNode[T]: ClassTag](rule: PartialFunction[T, T]) extends TreeRewriter[T, T] {
+case class BottomUp[T <: TreeNode[T] : ClassTag](rule: PartialFunction[T, T]) extends TreeRewriter[T] {
 
   def rewrite(tree: T): T = {
     val childrenLength = tree.children.length
@@ -63,7 +66,7 @@ case class BottomUp[T <: TreeNode[T]: ClassTag](rule: PartialFunction[T, T]) ext
   *
   * @note Note the applied rule cannot insert new parent nodes.
   */
-case class TopDown[T <: TreeNode[T]: ClassTag](rule: PartialFunction[T, T]) extends TreeRewriter[T, T] {
+case class TopDown[T <: TreeNode[T] : ClassTag](rule: PartialFunction[T, T]) extends TreeRewriter[T] {
 
   def rewrite(tree: T): T = {
     val afterSelf = if (rule.isDefinedAt(tree)) rule(tree) else tree
@@ -81,6 +84,34 @@ case class TopDown[T <: TreeNode[T]: ClassTag](rule: PartialFunction[T, T]) exte
         childrenCopy
       }
       afterSelf.withNewChildren(updatedChildren)
+    }
+  }
+
+}
+
+/**
+  * Applies the given transformation starting from the leafs of this tree.
+  */
+case class Transform[I <: TreeNode[I] : ClassTag, O](
+  transform: (I, List[O]) => O
+) extends TreeTransformer[I, O] {
+
+  def rewrite(tree: I): O = {
+    val children = tree.children
+    val childrenLength = children.length
+    if (childrenLength == 0) {
+      transform(tree, List.empty[O])
+    } else {
+      val transformedChildren = {
+        val tmpChildren = new Array[O](childrenLength)
+        var i = 0
+        while (i < childrenLength) {
+          tmpChildren(i) = rewrite(children(i))
+          i += 1
+        }
+        tmpChildren.toList
+      }
+      transform(tree, transformedChildren)
     }
   }
 
