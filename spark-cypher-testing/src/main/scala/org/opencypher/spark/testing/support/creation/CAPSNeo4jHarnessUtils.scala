10f07fbf393ea02209445b56a220dc8a440109ce
DarthMax <max.kiessling@neo4j.com>
2018-08-03 14:55:33 +0200

    Fix CAPSNeo4jHarnessUtils

--- spark-cypher-testing/src/main/scala/org/opencypher/spark/testing/support/creation/CAPSNeo4jHarnessUtils.scala
+++ spark-cypher-testing/src/main/scala/org/opencypher/spark/testing/support/creation/CAPSNeo4jHarnessUtils.scala
@@ -27,11 +27,11 @@
 package org.opencypher.spark.testing.support.creation
 
 import org.neo4j.harness.ServerControls
-import org.neo4j.kernel.builtinprocs.SchemaProcedure
 import org.opencypher.okapi.neo4j.io.testing.Neo4jHarnessUtils._
+import org.opencypher.okapi.procedures.OkapiProcedures
 
 object CAPSNeo4jHarnessUtils {
   implicit class CAPSServerControls(val neo4j: ServerControls) extends AnyVal {
-    def withSchemaProcedure = neo4j.withProcedure(classOf[SchemaProcedure])
+    def withSchemaProcedure = neo4j.withProcedure(classOf[OkapiProcedures])
   }
 }
