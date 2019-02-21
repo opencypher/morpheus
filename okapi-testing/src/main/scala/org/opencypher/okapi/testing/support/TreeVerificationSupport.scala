3a039d251734f3ccbac5f9acd6d8e95b3dab6021
DarthMax <max.kiessling@neo4j.com>
2019-02-21 11:19:10 +0100

    Add LogicalOptimizer tests for PatternScan injection
    Co-authored-by: Sören Reichardt <soren.reichardt@neotechnology.com>

---
+++ okapi-testing/src/main/scala/org/opencypher/okapi/testing/support/TreeVerificationSupport.scala
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2016-2019 "Neo4j Sweden, AB" [https://neo4j.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Attribution Notice under the terms of the Apache License 2.0
+ *
+ * This work was created by the collective efforts of the openCypher community.
+ * Without limiting the terms of Section 6, any Derivative Work that is not
+ * approved by the public consensus process of the openCypher Implementers Group
+ * should not be described as “Cypher” (and Cypher® is a registered trademark of
+ * Neo4j Inc.) or as "openCypher". Extensions by implementers or prototypes or
+ * proposals for change that have been documented or implemented should only be
+ * described as "implementation extensions to Cypher" or as "proposed changes to
+ * Cypher that are not yet approved by the openCypher community".
+ */
+package org.opencypher.okapi.testing.support
+
+import org.opencypher.okapi.testing.BaseTestSuite
+import org.opencypher.okapi.trees.TreeNode
+
+import scala.reflect.ClassTag
+
+trait TreeVerificationSupport {
+  self: BaseTestSuite =>
+
+  implicit class TreeNodeTestsUtils[T <: TreeNode[T]](op: T) {
+    def occourences[B <: TreeNode[T] : ClassTag]: Int = {
+      op.transform[Int] {
+        case (_: B, list) => list.sum + 1
+        case (_, list) => list.sum
+      }
+    }
+  }
+}
