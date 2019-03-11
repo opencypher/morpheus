21049acfc051e95b8a6224274da17d19e534d398
Tobias Johansson <tobias.j.e.johansson@gmail.com>
2019-03-11 10:24:23 +0100

    Add tasks for launching neo4j in separate jvm

---
+++ spark-cypher-testing/src/services/scala/org/opencypher/testing/services/Neo4j.scala
@@ -0,0 +1,15 @@
+package org.opencypher.testing.services
+
+import org.neo4j.harness.TestServerBuilders
+
+object Neo4j extends App {
+  val server = TestServerBuilders.newInProcessBuilder()
+    .withConfig("dbms.connector.bolt.listen_address", "127.0.0.1:7687")
+    .withConfig("dbms.connector.http.listen_address", "127.0.0.1:7474")
+    .newServer()
+
+  println(s"bolt: ${server.boltURI()}")
+  println(s"http: ${server.httpURI()}")
+  println("neo4j started")
+
+}
