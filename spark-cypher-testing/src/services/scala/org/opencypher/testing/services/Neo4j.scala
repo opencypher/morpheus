03456b7396a44347ef055f190a5698b7d86919da
Tobias Johansson <tobias.j.e.johansson@gmail.com>
2019-03-11 10:31:49 +0100

    Factor out the marker for service started

--- spark-cypher-testing/src/services/scala/org/opencypher/testing/services/Neo4j.scala
+++ spark-cypher-testing/src/services/scala/org/opencypher/testing/services/Neo4j.scala
@@ -3,6 +3,7 @@ package org.opencypher.testing.services
 import org.neo4j.harness.TestServerBuilders
 
 object Neo4j extends App {
+  val marker = args.headOption
   val server = TestServerBuilders.newInProcessBuilder()
     .withConfig("dbms.connector.bolt.listen_address", "127.0.0.1:7687")
     .withConfig("dbms.connector.http.listen_address", "127.0.0.1:7474")
@@ -10,6 +11,6 @@ object Neo4j extends App {
 
   println(s"bolt: ${server.boltURI()}")
   println(s"http: ${server.httpURI()}")
-  println("neo4j started")
+  marker.foreach(println)
 
 }
