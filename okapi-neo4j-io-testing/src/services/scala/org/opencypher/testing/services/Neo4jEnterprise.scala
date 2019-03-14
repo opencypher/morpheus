541e28b7ce0c9c2fd0518ac25895ce139d62ff94
Tobias Johansson <tobias.j.e.johansson@gmail.com>
2019-03-14 14:25:25 +0100

    Rewrite launcher tasks
    - Enterprise fails if instances > 1, so launch Community in that case
    - Extract tasks into method for code sharing
    - Add status task

--- okapi-neo4j-io-testing/src/services/scala/org/opencypher/testing/services/Neo4jEnterprise.scala
+++ okapi-neo4j-io-testing/src/services/scala/org/opencypher/testing/services/Neo4jEnterprise.scala
@@ -26,23 +26,8 @@
  */
 package org.opencypher.testing.services
 
-import org.neo4j.harness.EnterpriseTestServerBuilders
+import org.neo4j.harness.{EnterpriseTestServerBuilders, TestServerBuilder}
 
-object Neo4j extends App {
-  val Array(marker, instances) = args
-
-  Range(0, instances.toInt).foreach { i =>
-    val bolt = 7687 + i
-    val http = 7474 + i
-    val server = EnterpriseTestServerBuilders.newInProcessBuilder()
-      .withConfig("dbms.connector.bolt.listen_address", s"127.0.0.1:$bolt")
-      .withConfig("dbms.connector.http.listen_address", s"127.0.0.1:$http")
-      .newServer()
-
-    println(s"bolt: ${server.boltURI()}")
-    println(s"http: ${server.httpURI()}")
-  }
-
-  println(marker)
-
+object Neo4jEnterprise extends Neo4jLauncher {
+  override def builder: TestServerBuilder = EnterpriseTestServerBuilders.newInProcessBuilder()
 }
