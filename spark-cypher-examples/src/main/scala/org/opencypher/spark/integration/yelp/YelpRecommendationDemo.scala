aaa4083abb46d2b5871d3d42c53e60e3f84f7f9d
Martin Junghanns <martin.junghanns@neotechnology.com>
2019-03-27 10:23:10 +0100

    Read updated graph from Neo into Morpheus and compute top businesses
    Co-authored-by: Philip Stutz <philip.stutz@gmail.com>

--- spark-cypher-examples/src/main/scala/org/opencypher/spark/integration/yelp/YelpRecommendationDemo.scala
+++ spark-cypher-examples/src/main/scala/org/opencypher/spark/integration/yelp/YelpRecommendationDemo.scala
@@ -29,9 +29,11 @@ package org.opencypher.spark.integration.yelp
 import java.net.URI
 
 import org.apache.spark.sql.SparkSession
+import org.opencypher.okapi.impl.util.Measurement
 import org.opencypher.okapi.neo4j.io.Neo4jConfig
 import org.opencypher.spark.api.{CAPSSession, GraphSources}
 import org.opencypher.spark.integration.yelp.YelpOptions._
+import org.opencypher.okapi.neo4j.io.Neo4jHelpers._
 
 object YelpRecommendationDemo extends App {
 
@@ -40,34 +42,60 @@ object YelpRecommendationDemo extends App {
   implicit val caps: CAPSSession = CAPSSession.local()
   implicit val spark: SparkSession = caps.sparkSession
 
-  run(inputPath)
+  private val neo4jConfig = Neo4jConfig(new URI("bolt://localhost:7687"), "neo4j", Some("yelp"))
+
+  Measurement.printTiming("Analytical workflow")(run(inputPath))
 
   def run(path: String): Unit = {
 
-    val yelpGraph = GraphSources.fs(path).parquet.graph(yelpGraphName)
+    // Create Property Graph Data Sources
+    val parquetPgds = GraphSources.fs(path).parquet
+    val neo4jPgds = GraphSources.cypher.neo4j(neo4jConfig)
 
-    // Phoenix:   (18766 rows)
-    // Las Vegas: (29370 rows)
-    //    yelpGraph.cypher(
-    //      """
-    //        |MATCH (b:Business)
-    //        |WHERE b.city = 'Phoenix'
-    //        |RETURN b.name, b.city, b.state
-    //      """.stripMargin).show
+    // Load Yelp graph from Parquet (lazy)
+    val yelpGraph = parquetPgds.graph(yelpGraphName)
 
+    // Compute Phoenix subgraph containing business and their reviews
     val phoenixGraph = yelpGraph.cypher(
       """
         |MATCH (b:Business)<-[r:REVIEWS]-(user:User)
         |WHERE b.city = 'Phoenix'
         |CONSTRUCT
-        | CREATE (b)-[:REVIEWED_BY]->(user)
         | CREATE (user)-[COPY OF r]->(b)
         |RETURN GRAPH
       """.stripMargin).graph
 
-    GraphSources
-      .cypher
-      .neo4j(Neo4jConfig(new URI("bolt://localhost:7687"), "neo4j", Some("yelp")))
-      .store(yelpGraphName, phoenixGraph)
+    // Write Phoenix graph to Neo4j
+    neo4jPgds.store(yelpGraphName, phoenixGraph)
+
+    // Compute PageRank using Neo4j Graph Algorithms
+    val pageRankStats = neo4jConfig.cypherWithNewSession(
+      """
+        |CALL algo.pageRank(null, null, {
+        |  iterations:20,
+        |  dampingFactor:0.85,
+        |  direction: "BOTH",
+        |  write: true,
+        |  writeProperty:"pageRank",
+        |  weightProperty: "stars"
+        |})
+        |YIELD nodes, iterations, loadMillis, computeMillis, writeMillis, dampingFactor, write, writeProperty
+      """.stripMargin)
+
+    println(pageRankStats)
+
+    // Read updated graph from Neo4j
+    val phoenixGraphWithPageRank = GraphSources.cypher.neo4j(neo4jConfig).graph(yelpGraphName)
+
+    // Compute top businesses
+    val topBusinesses = phoenixGraphWithPageRank.cypher(
+      """
+        |MATCH (b:Business)
+        |RETURN b.name, b.pageRank
+        |ORDER BY b.pageRank DESC
+        |LIMIT 10
+      """.stripMargin)
+
+    topBusinesses.show
   }
 }
